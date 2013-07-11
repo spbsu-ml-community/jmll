ffc25ebc4fb3127a03f450fd166f46534341618d
towelenee <towelenee@yandex-team.ru>
2013-07-11 19:43:29 +0000

    CreedyContiousObliviousTree LQ added, but still buggy.

--- ml/src/com/spbsu/ml/methods/GreedyContinousObliviousTree.java
+++ ml/src/com/spbsu/ml/methods/GreedyContinousObliviousTree.java
@@ -1,25 +1,14 @@
 package com.spbsu.ml.methods;
 
 import com.spbsu.commons.math.vectors.Mx;
-import com.spbsu.commons.math.vectors.Vec;
 import com.spbsu.commons.math.vectors.VecTools;
 import com.spbsu.commons.math.vectors.impl.VecBasedMx;
-import com.spbsu.commons.util.ArrayTools;
 import com.spbsu.ml.BFGrid;
 import com.spbsu.ml.Oracle1;
 import com.spbsu.ml.data.DataSet;
-import com.spbsu.ml.data.Histogram;
-import com.spbsu.ml.data.impl.Bootstrap;
-import com.spbsu.ml.loss.L2Loss;
-import com.spbsu.ml.models.ObliviousTree;
-import com.spbsu.ml.optimization.ConvexFunction;
-import com.spbsu.ml.optimization.QuadraticFunction;
-import gnu.trove.TDoubleDoubleProcedure;
-import gnu.trove.TIntArrayList;
-import com.spbsu.ml.optimization.QuadraticFunction;
 import com.spbsu.ml.models.ContinousObliviousTree;
+import com.spbsu.ml.models.ObliviousTree;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -40,9 +29,13 @@ public class GreedyContinousObliviousTree extends GreedyTDRegion {
         this.depth = depth;
     }
 
-    public double[] createGradientCondition(DataSet ds, int mask, int col, int row) {
+    int numOfBondaries;
+    double[] right;
+
+    public void createGradientCondition(DataSet ds, int mask, int col, int row, Mx mx) {
         int n = depth + 1;
         double cond[] = new double[(1 << depth) * n * n];
+        double rightPart = 0;
         for (int k = 0; k < ds.data().columns(); k++) {
             double X = (col == 0 ? 1 : ds.data().get(k, col - 1)) *
                     (row == 0 ? 1 : ds.data().get(k, row - 1));
@@ -53,14 +46,21 @@ public class GreedyContinousObliviousTree extends GreedyTDRegion {
                     else
                         cond[mask * n * n + i * n + j] += X * (i == 0 ? 1 : ds.data().get(k, i - 1)) *
                                 (j == 0 ? 1 : ds.data().get(k, j - 1));
-            double rightPart = X * ds.target().get(k);     //Contant part of equation
+            rightPart += X * ds.target().get(k);     //Contant part of equation
         }
-        return cond;
+        addBoundary(mx, cond, rightPart);
     }
 
-    public void createBoundariesCondition(DataSet ds, int mask, BFGrid.BinaryFeature feature, int featureNum) {
+    public void addBoundary(Mx mx, double cond[], double rightPart) {
+        for (int i = 0; i < cond.length; i++)
+            mx.set(numOfBondaries, i, cond[i]);
+        right[numOfBondaries] = rightPart;
+        numOfBondaries++;
+    }
+
+    public void createBoundariesCondition(DataSet ds, int mask, BFGrid.BinaryFeature feature, int featureNum, Mx mx) {
         int n = depth + 1;
-        if(((mask >> featureNum) & 1) == 0)
+        if (((mask >> featureNum) & 1) == 0)
             return;
         int conterMask = mask ^ (1 << (featureNum));
         featureNum++;
@@ -70,19 +70,20 @@ public class GreedyContinousObliviousTree extends GreedyTDRegion {
             double cond[] = new double[(1 << depth) * n * n];
             cond[mask * n * n] = 1;
             cond[conterMask * n * n] = -1;
-            cond[mask * n * n + featureNum * n + featureNum] =  C * C;
+            cond[mask * n * n + featureNum * n + featureNum] = C * C;
             cond[conterMask * n * n + featureNum * n + featureNum] = -C * C;
-
+            addBoundary(mx, cond, 0);
         }
         //linear condition
         for (int i = 0; i < n; i++)
-            if(i != featureNum){
+            if (i != featureNum) {
                 double cond[] = new double[(1 << depth) * n * n];
                 cond[mask * n * n + i * n] = cond[mask * n * n + i] = 0.5;
                 cond[conterMask * n * n + i * n] = cond[conterMask * n * n + i] = -0.5;
-                cond[mask * n * n + i * n + featureNum] = cond[mask * n * n + featureNum * n+ i] = C / 2;
+                cond[mask * n * n + i * n + featureNum] = cond[mask * n * n + featureNum * n + i] = C / 2;
                 cond[conterMask * n * n + i * n + featureNum] =
-                        cond[conterMask * n * n + featureNum * n+ i] = -C / 2;
+                        cond[conterMask * n * n + featureNum * n + i] = -C / 2;
+                addBoundary(mx, cond, 0);
             }
 
         //Quadratic condition
@@ -92,6 +93,7 @@ public class GreedyContinousObliviousTree extends GreedyTDRegion {
                     double cond[] = new double[(1 << depth) * n * n];
                     cond[mask * n * n + i * n + i] = C * C;
                     cond[conterMask * n * n + i * n + i] = -C * C;
+                    addBoundary(mx, cond, 0);
                 }
     }
 
@@ -107,20 +109,59 @@ public class GreedyContinousObliviousTree extends GreedyTDRegion {
         double value[][] = new double[1 << features.size()][(features.size() + 1) * (features.size() + 1)];
         int cnt[] = new int[1 << depth];
         int n = features.size() + 1;
-        //Optimal for miss
+        int condFor = (2 * n * n + features.size() * (n) * (n - 1) + 1);
+        int dim = (1 << depth) * condFor / 2;
+        if (depth == 1)
+            dim = 10;
+        else if (depth == 2)
+            dim = 52;
+        Mx mx = new VecBasedMx(dim, (1 << depth) * n * n);
+        right = new double[dim];
+        Mx A = new VecBasedMx(dim, (1 << depth) * n * n);
+        Mx Q = new VecBasedMx(dim, (1 << depth) * n * n);
+        numOfBondaries = 0;
         for (int mask = 0; mask < 1 << depth; mask++) {
+            //Optimal for miss
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++) {
-                    createGradientCondition(ds, mask, i, j);
+                    createGradientCondition(ds, mask, i, j, mx);
                 }
-            for(int i = 0;i < features.size();i++)
-                createBoundariesCondition(ds,mask,features.get(i),i);
+            /*for (int i = 0; i < features.size(); i++)
+                createBoundariesCondition(ds, mask, features.get(i), i, mx);*/
         }
+        System.out.println(mx);
+        System.out.print("Created bounaries");
+        System.out.println(numOfBondaries + " " + right.length);
+        VecTools.householderLQ(mx, A, Q);
+        double solution[] = new double[(1 << depth) * n * n];
+
+        /*System.out.print(Q.toString());
+        System.out.println("next");
+        System.out.print(mx.toString());
+        System.out.println("next");
+        System.out.print(A.toString());*/
+
+        for (int i = (1 << depth) * n * n - 1; i >= 0; i--) {
+            solution[i] = right[i];
+            //System.out.println(right[i] + " " + Q.get(i,i));
+            for (int j = i + 1; j < (1 << depth) * n * n; j++)
+                solution[i] -= solution[j] * Q.get(i, j);
+            if (Q.get(i, i) != 0)
+                solution[i] /= Q.get(i, i);
+            else
+                solution[i] = 0;
+        }
+        for (int i = 0; i < 1 << depth; i++)
+            for (int j = 0; j < n * n; j++) {
+                value[i][j] = solution[i * n * n + j];
+                //System.out.println(i + " " + j + "=" + value[i][j]);
+            }
+        //System.out.print(B.toString());
         return new ContinousObliviousTree(features, value);
     }
 
 
-    private class BestBFFinder implements TDoubleDoubleProcedure {
+   /* private class BestBFFinder implements TDoubleDoubleProcedure {
         double score = 0;
         int fold = 0;
         double bestScore = Double.MAX_VALUE;
@@ -160,5 +201,5 @@ public class GreedyContinousObliviousTree extends GreedyTDRegion {
         public int bestSplit() {
             return bestFeature;
         }
-    }
+    } */
 }
