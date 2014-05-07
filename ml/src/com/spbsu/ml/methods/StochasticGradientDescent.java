aece15045080853ca7c9b6b93301af7942bc56f2
qdeee <qdeee@yandex-team.ru>
2014-05-07 19:23:40 +0400

    hotfix: merge error

--- ml/src/com/spbsu/ml/methods/StochasticGradientDescent.java
+++ ml/src/com/spbsu/ml/methods/StochasticGradientDescent.java
@@ -7,6 +7,7 @@ import com.spbsu.commons.util.ArrayTools;
 import com.spbsu.ml.FuncC1;
 import com.spbsu.ml.func.FuncEnsemble;
 import com.spbsu.ml.optimization.Optimize;
+import com.sun.javaws.exceptions.InvalidArgumentException;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
 
@@ -16,7 +17,7 @@ import java.util.Random;
  * User: qdeee
  * Date: 17.03.14
  */
-public class StochasticGradientDescent implements Optimize<FuncEnsemble<FuncC1>> {
+public class StochasticGradientDescent implements Optimize<FuncEnsemble> {
   private final int iterations;
   private final double step;
   private Random rand;
@@ -28,7 +29,11 @@ public class StochasticGradientDescent implements Optimize<FuncEnsemble<FuncC1>>
   }
 
   @Override
-  public Vec optimize(final FuncEnsemble<FuncC1> sumFuncs) {
+  public Vec optimize(final FuncEnsemble sumFuncs) {
+    if (sumFuncs.last().gradient() == null) {
+      throw new IllegalArgumentException("Internal functions must implement not-null gradient()");
+    }
+
     Vec x = new ArrayVec(sumFuncs.xdim());
     for (int i = 0; i < x.dim(); i++) {
       x.set(i, rand.nextGaussian());
@@ -39,7 +44,7 @@ public class StochasticGradientDescent implements Optimize<FuncEnsemble<FuncC1>>
     while (iter++ < iterations) {
       indices.shuffle(rand);
       for (int i = 0; i < indices.size(); i++) {
-        VecTools.incscale(x, sumFuncs.models[i].gradient(x), -step);
+        VecTools.incscale(x, sumFuncs.models[i].gradient().trans(x), -step);
       }
     }
     return x;
