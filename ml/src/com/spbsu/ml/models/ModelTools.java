74eb8f21edd300c84e789c19e98e28d7b3de55a3
Igor E. Kuralenok <solar@yandex-team.ru>
2014-05-20 18:41:41 +0400

    Corrected OT compilation

--- ml/src/com/spbsu/ml/models/ModelTools.java
+++ ml/src/com/spbsu/ml/models/ModelTools.java
@@ -2,9 +2,12 @@ package com.spbsu.ml.models;
 
 import com.spbsu.commons.math.MathTools;
 import com.spbsu.commons.math.vectors.Vec;
+import com.spbsu.commons.util.Pair;
 import com.spbsu.ml.BFGrid;
 import com.spbsu.ml.Func;
 import com.spbsu.ml.func.Ensemble;
+import com.spbsu.ml.models.pgm.ProbabilisticGraphicalModel;
+import gnu.trove.list.array.TDoubleArrayList;
 import gnu.trove.map.hash.TObjectDoubleHashMap;
 import gnu.trove.procedure.TObjectDoubleProcedure;
 import gnu.trove.set.TIntSet;
@@ -17,9 +20,31 @@ import java.util.*;
  * Time: 8:31
  */
 public class ModelTools {
+  private static class ConditionEntry{
+    public final int[] features;
+
+    private ConditionEntry(int[] features) {
+      this.features = features;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+
+      ConditionEntry that = (ConditionEntry) o;
+      return Arrays.equals(features, that.features);
+    }
+
+    @Override
+    public int hashCode() {
+      return Arrays.hashCode(features);
+    }
+  }
   public static Func compile(Ensemble<ObliviousTree> input) {
     final BFGrid grid = input.size() > 0 ? input.models[0].grid() : null;
-    final TObjectDoubleHashMap<Set<BFGrid.BinaryFeature>> scores = new TObjectDoubleHashMap<Set<BFGrid.BinaryFeature>>();
+    final TObjectDoubleHashMap<ConditionEntry> scores = new TObjectDoubleHashMap<ConditionEntry>();
     for (int i = 0; i < input.size(); i++) {
       final ObliviousTree model = input.models[i];
       final List<BFGrid.BinaryFeature> features = model.features();
@@ -30,15 +55,64 @@ public class ModelTools {
         int bitsB = MathTools.bits(b);
         for (int a = 0; a < values.length; a++) {
           int bitsA = MathTools.bits(a);
-          value += MathTools.bits(a & b) >= bitsA ? (((bitsA + bitsB) & 1) > 0 ? -1 : 1) * values[a] : 0;
+          if (MathTools.bits(a & b) >= bitsA)
+            value += (((bitsA + bitsB) & 1) > 0 ? -1 : 1) * values[a];
         }
         for (int f = 0; f < features.size(); f++) {
           if ((b >> f & 1) != 0) {
             currentSet.add(features.get(features.size() - f - 1));
           }
         }
-        scores.adjustOrPutValue(currentSet, value, value);
+        if (value != 0.) {
+          int[] currentSetA = new int[currentSet.size()];
+          final Iterator<BFGrid.BinaryFeature> it = currentSet.iterator();
+          for (int j = 0; j < currentSetA.length; j++) {
+            currentSetA[j] = it.next().bfIndex;
+          }
+          Arrays.sort(currentSetA);
+          final ConditionEntry entry = new ConditionEntry(currentSetA);
+          if (scores.containsKey(entry))
+            scores.adjustValue(entry, input.weights.get(i) * value);
+          else
+            scores.put(entry, input.weights.get(i) * value);
+        }
       }
+    }
+    final List<Pair<ConditionEntry, Double>> sortedScores = new ArrayList<Pair<ConditionEntry, Double>>();
+
+    scores.forEachEntry(new TObjectDoubleProcedure<ConditionEntry>() {
+      @Override
+      public boolean execute(ConditionEntry a, double b) {
+        sortedScores.add(Pair.create(a, b));
+        return true;
+      }
+    });
+    Collections.sort(sortedScores, new Comparator<Pair<ConditionEntry, Double>>() {
+      @Override
+      public int compare(Pair<ConditionEntry, Double> o1, Pair<ConditionEntry, Double> o2) {
+        if (o1.first.features.length > o2.first.features.length)
+          return 1;
+        else if (o1.first.features.length == o2.first.features.length) {
+          int index = 0;
+          while (o1.first.features[index] == o2.first.features[index])
+            index++;
+          return Integer.compare(o1.first.features[index], o2.first.features[index]);
+        }
+
+        return -1;
+      }
+    });
+
+    final List<int[]> entries = new ArrayList<int[]>(scores.size());
+    final TDoubleArrayList values = new TDoubleArrayList(scores.size());
+    int[] freqs = new int[grid.size()];
+
+    for (Pair<ConditionEntry, Double> score : sortedScores) {
+      entries.add(score.first.features);
+      for (int i = 0; i < score.first.features.length; i++) {
+        freqs[score.first.features[i]]++;
+      }
+      values.add(score.second);
     }
     return new Func.Stub() {
       byte[] binary = new byte[grid.rows()];
@@ -48,19 +122,19 @@ public class ModelTools {
           return 0.;
 
         grid.binarize(x, binary);
-        final double[] result = new double[]{0.};
-        scores.forEachEntry(new TObjectDoubleProcedure<Set<BFGrid.BinaryFeature>>() {
-          @Override
-          public boolean execute(Set<BFGrid.BinaryFeature> a, double b) {
-            for (BFGrid.BinaryFeature bf : a) {
-              if (!bf.value(binary))
-                return true;
+        double result = 0.;
+        for (int i = 0; i < entries.size(); i++) {
+          final int[] conditions = entries.get(i);
+          double increment = values.get(i);
+          for (int j = 0; j < conditions.length; j++) {
+            if (!grid.bf(conditions[j]).value(binary)) {
+              increment = 0;
+              break;
             }
-            result[0] += b;
-            return true;
           }
-        });
-        return result[0];
+          result += increment;
+        }
+        return result;
       }
 
       @Override
