abc16ddc9c6991a73fe9e62f1460462185b70b58
Igor E. Kuralenok <solar@yandex-team.ru>
2014-06-20 16:19:10 +0400

    Vectorization

--- ml/src/com/spbsu/ml/Vectorization.java
+++ ml/src/com/spbsu/ml/Vectorization.java
@@ -1,9 +1,13 @@
 package com.spbsu.ml;
 
+import com.spbsu.commons.math.vectors.Vec;
+
 /**
  * User: solar
  * Date: 20.06.14
  * Time: 15:07
  */
-public class Vectorization {
+public interface Vectorization<T> {
+  Vec value(T subject);
+  int dim();
 }
