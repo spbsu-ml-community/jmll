98d0fd31b9f71a603028735bb76a93603d54019e
Igor E. Kuralenok <solar@yandex-team.ru>
2014-07-23 19:45:33 +0400

    Better alignment

--- ml/src/main/java/com/spbsu/ml/meta/items/ViewportAnswersWeighting.java
+++ ml/src/main/java/com/spbsu/ml/meta/items/ViewportAnswersWeighting.java
@@ -1,5 +1,7 @@
 package com.spbsu.ml.meta.items;
 
+import com.spbsu.commons.math.vectors.VecIterator;
+import com.spbsu.commons.math.vectors.VecTools;
 import com.spbsu.commons.math.vectors.impl.vectors.DVector;
 import com.spbsu.ml.meta.DSItem;
 
@@ -9,13 +11,22 @@ import com.spbsu.ml.meta.DSItem;
  * Time: 17:01
  */
 public class ViewportAnswersWeighting implements DSItem {
-  public String vpName;
-  public DVector<String> answers;
   public String reqId;
+  public String vpName;
+  public String[] answers;
+  public double[] weights;
 
   public ViewportAnswersWeighting(final String reqId, String vpName, final DVector<String> answers) {
     this.vpName = vpName;
-    this.answers = answers;
+    final VecIterator nzIt = answers.nonZeroes();
+    this.answers = new String[VecTools.l0(answers)];
+    this.weights = new double[this.answers.length];
+    int index = 0;
+    while (nzIt.advance()) {
+      this.answers[index] = answers.basis().fromIndex(nzIt.index());
+      this.weights[index] = nzIt.value();
+      index++;
+    }
     this.reqId = reqId;
   }
 
