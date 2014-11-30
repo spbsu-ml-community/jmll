ba1235a6004529c86639f7cac4fa955f299e6649
noxoomo <noxoomo@yandex-team.ru>
2014-11-30 10:54:20 +0300

    hiercherry region refactor

--- ml/src/main/java/com/spbsu/ml/methods/greedyRegion/hierarchicalCherryRegion/GreedyHierarchicalRegion.java
+++ ml/src/main/java/com/spbsu/ml/methods/greedyRegion/hierarchicalCherryRegion/GreedyHierarchicalRegion.java
@@ -2,7 +2,6 @@ package com.spbsu.ml.methods.greedyRegion.hierarchicalCherryRegion;
 
 import com.spbsu.commons.func.AdditiveStatistics;
 import com.spbsu.commons.func.Evaluator;
-import com.spbsu.commons.math.vectors.Vec;
 import com.spbsu.commons.util.ArrayTools;
 import com.spbsu.ml.BFGrid;
 import com.spbsu.ml.Binarize;
@@ -10,23 +9,29 @@ import com.spbsu.ml.data.impl.BinarizedDataSet;
 import com.spbsu.ml.data.set.VecDataSet;
 import com.spbsu.ml.loss.StatBasedLoss;
 import com.spbsu.ml.methods.VecOptimization;
+import com.spbsu.ml.methods.greedyRegion.AdditiveStatisticsExtractors;
 import com.spbsu.ml.models.CherryRegion;
 
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 
+import static com.spbsu.ml.methods.greedyRegion.AdditiveStatisticsExtractors.sum;
+import static com.spbsu.ml.methods.greedyRegion.AdditiveStatisticsExtractors.weight;
+
 /**
  * Created by noxoomo on 24/11/14.
  */
 public class GreedyHierarchicalRegion<Loss extends StatBasedLoss> extends VecOptimization.Stub<Loss> {
   protected final BFGrid grid;
+  int[][] useCounts;
 
-
-  public GreedyHierarchicalRegion (BFGrid grid) {
+  public GreedyHierarchicalRegion(BFGrid grid) {
     this.grid = grid;
+    useCounts = new int[grid.rows()][];
+    for (int f = 0; f < grid.rows(); ++f)
+      useCounts[f] = new int[grid.row(f).size() + 1];
   }
-
 
 
   @Override
@@ -34,44 +39,62 @@ public class GreedyHierarchicalRegion<Loss extends StatBasedLoss> extends VecOpt
     final List<RegionLayer> layers = new ArrayList<>(10);
     final BinarizedDataSet bds = learn.cache().cache(Binarize.class, VecDataSet.class).binarize(grid);
 
-    HierarchicalPick pick = new HierarchicalPick(bds,loss.statsFactory(),7,4.0);
+
+    HierarchicalPick pick = new HierarchicalPick(bds, loss.statsFactory(), 10, 14, useCounts);
     int[] points = ArrayTools.sequence(0, learn.length());
 
     double information = 0;
     double currentScore = Double.POSITIVE_INFINITY;
+    BitSet used = new BitSet();
     while (true) {
       RegionLayer layer = pick.build(new Evaluator<AdditiveStatistics>() {
         @Override
         public double value(AdditiveStatistics stat) {
+          if (stat == null)
+            return Double.POSITIVE_INFINITY;
           return loss.score(stat);
         }
       }, points);
-      if (currentScore * (1.0 / (1 +information + Math.log(layers.size()+1))) <=loss.score(layer.inside)* (1.0 / (1 + layer.information + information + Math.log(layers.size()+2))) + 1e-9) {// * (1 + 1.0 / (1  + Math.log(layers.size()+2))) + 1e-9) {
+
+      if (currentScore <= score(layer.inside) + 1e-9 || layer.information == Double.POSITIVE_INFINITY) {
         break;
       }
-      System.out.println("\n"  + layer.conditions);
+      System.out.println("\n" + layer.conditions);
+      System.out.println("\n" + weight(layer.inside));
       layers.add(layer);
-      currentScore = loss.score(layer.inside);//layer.score;// * (1 + 1.0 / (1  + Math.log(layers.size()+1)));
+      currentScore = score(layer.inside);
       information += layer.information;
+      int index = 0;
+      for (int f = 0; f < grid.rows(); ++f)
+        for (int bin = 0; bin <= grid.row(f).size(); ++bin, ++index)
+          if (layer.conditions.get(index))
+            useCounts[f][bin]++;
+      used.or(layer.conditions);
       points = layer.insidePoints;
     }
     BitSet[] conditions = new BitSet[layers.size()];
-    for (int i=0; i < conditions.length;++i) {
+    for (int i = 0; i < conditions.length; ++i) {
       conditions[i] = layers.get(i).conditions;
     }
 
-    CherryRegion region = new CherryRegion(conditions, 1, grid);
-    Vec target = loss.target();
-    double sum = 0;
-    double weight = 0;
-    for (int i = 0; i < bds.original().length(); ++i) {
-      if (region.value(bds, i) == 1) {
-        sum += target.get(i);
-        ++weight;
-      }
-    }
-    System.out.println("\nFound cherry region with " + weight + " points");
-//    return new CherryRegion(conditions.toArray(new BitSet[conditions.size()]), weight > 0 ? sum / weight : 0, grid);
-    return new CherryRegion(conditions, sum / weight,grid); //loss.bestIncrement(layers.get(layers.size()-1).inside), grid);
+//    CherryRegion region = new CherryRegion(conditions, 1, grid);
+//    Vec target = loss.target();
+//    double sum = 0;
+//    double weight = 0;
+//    for (int i = 0; i < bds.original().length(); ++i) {
+//      if (region.value(bds, i) == 1) {
+//        sum += target.get(i);
+//        ++weight;
+//      }
+//    }
+    AdditiveStatistics inside = layers.get(layers.size() - 1).inside;
+    System.out.println("\nFound cherry region with " + AdditiveStatisticsExtractors.weight(inside) + " bootstrap weight");
+    return new CherryRegion(conditions, loss.bestIncrement(inside), grid);
+  }
+
+  public double score(AdditiveStatistics stats) {
+    double sum = sum(stats);
+    double weight = weight(stats);
+    return weight > 1 ? (-sum * sum / weight) * weight * (weight - 2) / (weight * weight - 3 * weight + 1) * (1 + 2 * Math.log(weight)) : 0;
   }
 }
--- ml/src/main/java/com/spbsu/ml/methods/greedyRegion/hierarchicalCherryRegion/HierarchicalPick.java
+++ ml/src/main/java/com/spbsu/ml/methods/greedyRegion/hierarchicalCherryRegion/HierarchicalPick.java
@@ -18,7 +18,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 /**
  * Created by noxoomo on 24/11/14.
  */
-
+//TODO: RegionStats merge vs RegionLayer (add lazy points initialization)
 public class HierarchicalPick {
   private final BinarizedDataSet bds;
   private final BFGrid grid;
@@ -29,13 +29,15 @@ public class HierarchicalPick {
   final int take2grow;
   final CategoricalAggregate aggregate;
   double lambda;
+  final int[][] useCounts;
 
-  public HierarchicalPick(BinarizedDataSet bds, Factory<AdditiveStatistics> factory, int take2grow, double lambda) {
+  public HierarchicalPick(BinarizedDataSet bds, Factory<AdditiveStatistics> factory, int take2grow, double lambda, int[][] useCounts) {
     this.lambda = lambda;
     this.bds = bds;
     this.grid = bds.grid();
     this.factory = factory;
     this.binsIndex = new int[grid.rows()][];
+    this.useCounts = useCounts;
 
     int current = 0;
     for (int i = 0; i < grid.rows(); i++) {
@@ -50,19 +52,21 @@ public class HierarchicalPick {
     this.take2grow = take2grow;
   }
 
+
   private static final ThreadPoolExecutor exec = ThreadTools.createBGExecutor("Pick thread", -1);
 
 
+  BitSet used = new BitSet();
+
   public <T extends AdditiveStatistics> RegionLayer<T> build(final Evaluator<T> eval, int[] points) {
     if (points == null || points.length == 0) {
-      return null;
+      throw  new RuntimeException("error");
     }
+
     final ModelComplexityCalcer modelComplexity = new ModelComplexityCalcer(bds, binsIndex, points);
     ArrayList<RegionLayer<T>> models = init(eval, points, modelComplexity);
 
-    final StatsComparator statsComparator = new StatsComparator();
-    final ModelComparator modelsComparator = new ModelComparator();
-    RegionLayer resultModel = models.get(0);
+    RegionLayer resultModel = chooseBest(models,eval);
 
     do {
       ArrayList<RegionStats<T>> fittedModels = new ArrayList<>(models.size() * (1 + binsCount));
@@ -70,7 +74,6 @@ public class HierarchicalPick {
       for (RegionLayer layer : models) {
         final RegionLayer<T> finalLayer = layer;
         final RegionStats[] buildResult = build(new Evaluator<T>() {
-
           @Override
           public double value(T stat) {
             T totalStat = (T) factory.create();
@@ -82,25 +85,28 @@ public class HierarchicalPick {
 
         for (RegionStats stat : buildResult) {
           stat.basedOn = layer;
-          if (stat.information != Double.POSITIVE_INFINITY)
+          if (stat.information < lambda)
             fittedModels.add(stat);
         }
       }
 
-      Collections.sort(fittedModels, statsComparator);
-      models = clean(fittedModels);
-
-      RegionLayer bestModel = resultModel;
-      for (RegionLayer model : models) {
-        bestModel = chooseBest(bestModel, model);
-      }
-
-      if (bestModel == resultModel) {
+      models = takeGrowPart(fittedModels);
+      RegionLayer bestModel = chooseBest(models,eval);
+      bestModel = chooseBest(bestModel,resultModel,eval);
+      if (bestModel == resultModel || bestModel.score == Double.POSITIVE_INFINITY) {
         break;
       }
       resultModel = bestModel;
     } while (true);
 
+    used.or(resultModel.conditions);
+    return resultModel;
+  }
+
+  private <T extends AdditiveStatistics> RegionLayer chooseBest(ArrayList<RegionLayer<T>> models, Evaluator<T> eval) {
+    RegionLayer resultModel = models.get(0);
+    for (int i = 0; i < models.size(); ++i)
+      resultModel = chooseBest(resultModel, models.get(i),eval);
     return resultModel;
   }
 
@@ -109,31 +115,37 @@ public class HierarchicalPick {
     final RegionStats[] buildResult = build(eval, new BitSet(binsCount), points, calcer);
     ArrayList<RegionStats<T>> fittedModels = new ArrayList<>(buildResult.length);
     for (RegionStats stat : buildResult) {
-      stat.basedOn = fakeLayer;
-      fittedModels.add(stat);
+      if (stat.information < lambda) {
+        stat.basedOn = fakeLayer;
+        fittedModels.add(stat);
+      }
     }
-    return clean(fittedModels);
+    return takeGrowPart(fittedModels);
   }
 
-  private <T extends AdditiveStatistics> ArrayList<RegionLayer<T>> clean(ArrayList<RegionStats<T>> models) {
-    ArrayList<RegionLayer<T>> result = new ArrayList<>(take2grow + 1);
-    if (take2grow + 1 >= models.size()) {
+  private <T extends AdditiveStatistics> ArrayList<RegionLayer<T>> takeGrowPart(ArrayList<RegionStats<T>> models) {
+    ArrayList<RegionLayer<T>> result = new ArrayList<>(take2grow);
+
+    if (take2grow >= models.size()) {
       for (int i = 0; i < models.size(); ++i) {
         result.add(split(models.get(i)));
       }
       return result;
     }
 
+    Collections.sort(models, new Comparator<RegionStats<T>>() {
+      @Override
+      public int compare(RegionStats<T> o1, RegionStats<T> o2) {
+        if (o1.score == o2.score) {
+          return Double.compare(o1.information, o2.information);
+        }
+        return Double.compare(o1.score, o2.score);
+      }
+    });
+
     for (int i = 0; i < take2grow; ++i) {
       result.add(split(models.get(i)));
     }
-
-
-    RegionStats best = models.get(take2grow + 1);
-    for (int i = take2grow + 1; i < models.size(); ++i) {
-      best = chooseBest(best, models.get(i));
-    }
-    result.add(split(best));
     return result;
   }
 
@@ -155,67 +167,32 @@ public class HierarchicalPick {
     included.add(best.basedOn.insidePoints);
     BitSet conditions = (BitSet) best.basedOn.conditions.clone();
     conditions.set(binsIndex[best.feature][best.bin]);
-
-    if (included.size() + excluded.size() != best.basedOn.insidePoints.length + best.basedOn.outsidePoints.length) {
-      System.out.println("bug");
-    }
     return new RegionLayer<>(inside, conditions, best.information, best.score, included.toArray(), excluded.toArray());
   }
 
 
-  private RegionLayer chooseBest(RegionLayer bestModel, RegionLayer model) {
+  private RegionLayer chooseBest(RegionLayer bestModel, RegionLayer model,final Evaluator eval) {
     if (bestModel == null) {
       return model;
     }
-    if (bestModel.score * (1.0 / (1 + lambda * bestModel.information)) < model.score *(1.0 / (1 + lambda * model.information))) {
-//    if (bestModel.score * (1.0 - 8 * bestModel.information) < model.score * (1.0 - 8 * bestModel.information)) {
-//    if (bestModel.score < model.score || model.information == Double.POSITIVE_INFINITY) {
+    if (model.information == Double.POSITIVE_INFINITY) {
+      return bestModel;
+    }
+
+    if (eval.value(bestModel.inside) <= eval.value(model.inside) + 1e-9) {
       return bestModel;
     }
     return model;
   }
 
-  private RegionStats chooseBest(RegionStats bestModel, RegionStats model) {
-    if (bestModel == null) {
-      return model;
-    }
-    if (bestModel.score * (1.0 / (1 + lambda* bestModel.information)) < model.score *( 1.0 / (1 + lambda *model.information))) {
-//    if (bestModel.score * (1.0 - 8 * bestModel.information) < model.score * (1.0 - 8 * bestModel.information)) {
-//    if (bestModel.score < model.score || model.information == Double.POSITIVE_INFINITY) {
-      return bestModel;
-    }
-    return model;
-  }
-
-  class StatsComparator implements Comparator<RegionStats> {
-
-    @Override
-    public int compare(RegionStats first, RegionStats second) {
-      if (first.information < second.information) {
-        return -1;
-      } else if (first.information == second.information) {
-        return Double.compare(first.score, second.score);
-      }
-      return 1;
-    }
-  }
-
-  class ModelComparator implements Comparator<RegionLayer> {
-    @Override
-    public int compare(RegionLayer first, RegionLayer second) {
-      if (first.information < second.information) {
-        return -1;
-      } else if (first.information == second.information) {
-        return Double.compare(first.score, second.score);
-      }
-      return 1;
-    }
-  }
 
   @SuppressWarnings("unchecked")
   public <T extends AdditiveStatistics> RegionStats<T>[] build(final Evaluator<T> eval, final BitSet condition, final int[] points, final ModelComplexityCalcer modelComplexity) {
     final RegionStats<T> result[] = new RegionStats[binsCount - condition.cardinality()];
     final CountDownLatch latch = new CountDownLatch(result.length);
+    final T total = (T) factory.create();
+    for (int point : points)
+      total.append(point, 1);
 
     int index = 0;
     for (int f = 0; f < grid.rows(); ++f) {
@@ -224,9 +201,17 @@ public class HierarchicalPick {
         if (condition.get(binsIndex[f][bin])) {
           continue;
         }
+
+        if (used.get(binsIndex[f][bin])) { //skip features, used on previous layers
+          result[index] = new RegionStats<>((T) factory.create(), finalFeature, bin, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+          latch.countDown();
+          ++index;
+          continue;
+        }
+
         final int finalIndex = index;
-        ++index;
         final int finalBin = bin;
+        ++index;
         condition.set(binsIndex[f][bin]);
         final double complexity = modelComplexity.calculate(condition);
         condition.clear(binsIndex[f][bin]);
@@ -241,7 +226,8 @@ public class HierarchicalPick {
               if (finalBin == bin[index])
                 stat.append(index, 1);
             }
-            final double score = eval.value(stat);
+//            final double score = eval.value(stat) * (1.0 / (1 + Math.log(useCounts[finalFeature][finalBin] + 1))) * (1.0 / (1 + 0.005 * complexity));
+            final double score = eval.value(stat);// * (1.0 / (1 + Math.log(useCounts[finalFeature][finalBin] + 1))) * (1.0 / (1 + 0.005 * complexity));
             result[finalIndex] = new RegionStats<T>(stat, finalFeature, finalBin, complexity, score);
             latch.countDown();
           }
@@ -251,7 +237,6 @@ public class HierarchicalPick {
     try {
       latch.await();
     } catch (InterruptedException e) {
-
     }
     return result;
   }
@@ -261,6 +246,7 @@ public class HierarchicalPick {
     private final BFGrid grid;
     private final int[][] binsIndex;
     private final int[][] base;
+    public double total;
 
     public ModelComplexityCalcer(BinarizedDataSet bds, int[][] binsIndex, int[] points) {
       this.grid = bds.grid();
@@ -275,15 +261,14 @@ public class HierarchicalPick {
           }
         }
       }
+      total = 0;
+      for (int bin = 0; bin <= grid.row(0).size(); ++bin) {
+        total += base[0][bin];
+      }
     }
 
     public double calculate(BitSet conditions) {
       double bits = 0;
-      double total = 0;
-      for (int bin = 0; bin <= grid.row(0).size(); ++bin) {
-        total += base[0][bin];
-      }
-
       int usedFeatures = 0;
       for (int f = 0; f < grid.rows(); ++f) {
         boolean used = false;
@@ -294,83 +279,30 @@ public class HierarchicalPick {
         for (int bin = 0; bin <= grid.row(f).size(); ++bin) {
           if (conditions.get(binsIndex[f][bin]) == current) {
             count += base[f][bin];
-            if (base[f][bin] == 0 && conditions.get(binsIndex[f][bin])) {
-              entropy = Double.POSITIVE_INFINITY;
-            }
           } else {
             realCardinality++;
-            if (base[f][bin] == 0 && conditions.get(binsIndex[f][bin])) {
-              entropy = Double.POSITIVE_INFINITY;
-            }
             entropy += count > 0 ? count * Math.log(count) : 0;
             current = conditions.get(binsIndex[f][bin]);
             used = current ? current : used;
             count = base[f][bin];
           }
+          if (current && binsIndex[f][bin] == 0)
+            return Double.POSITIVE_INFINITY;
         }
         if (!used)
           continue;
         ++usedFeatures;
         realCardinality++;
-        if (entropy == Double.POSITIVE_INFINITY)
-          return Double.POSITIVE_INFINITY;
         entropy += count > 0 ? count * Math.log(count) : 0;
         entropy /= total;
         entropy = Math.log(total) - entropy;
-        bits += entropy + 32*Math.log(2 * realCardinality);
+        bits += realCardinality;
+        bits += entropy;
       }
-      bits += Math.log(usedFeatures) * Math.log(grid.rows());
+      bits += usedFeatures * Math.log(grid.rows());
       return bits;
     }
-
-//    public double calculate(BitSet conditions) {
-//      double result = 0;
-//      double total = 0;
-//      for (int bin = 0; bin <= grid.row(0).size(); ++bin) {
-//        total += base[0][bin];
-//      }
-//
-//      int usedFeatures = 0;
-//      for (int f = 0; f < grid.rows(); ++f) {
-//        boolean used = false;
-//        double entropy = 0;
-//        int realCardinality = 0;
-//        double count = 0;
-//        boolean current = false;
-//        for (int bin = 0; bin <= grid.row(f).size(); ++bin) {
-//          if (conditions.get(binsIndex[f][bin]) == current) {
-//            count += base[f][bin];
-//            if (base[f][bin] == 0 && conditions.get(binsIndex[f][bin])) {
-//              entropy = Double.NEGATIVE_INFINITY;
-//            }
-//          } else {
-//            realCardinality++;
-//            if (base[f][bin] == 0 && conditions.get(binsIndex[f][bin])) {
-//              entropy = Double.NEGATIVE_INFINITY;
-//            }
-//            entropy += count > 0 ? count * Math.log(count) : 0;
-//            current = conditions.get(binsIndex[f][bin]);
-//            used = current ? current : used;
-//            count = base[f][bin];
-//          }
-//        }
-//        if (!used)
-//          continue;
-//        ++usedFeatures;
-//        realCardinality++;
-//        if (entropy == Double.NEGATIVE_INFINITY)
-//          return Double.NEGATIVE_INFINITY;
-//        entropy += count > 0 ? count * Math.log(count) : 0;
-////        entropy /= total;
-////        entropy = Math.log(total) - entropy;
-//        result += entropy;// + Math.log(2 * realCardinality);
-//      }
-////      result += Math.log(usedFeatures) * Math.log(grid.rows());
-//      return -result;
-//    }
   }
-
-
 }
 
 
