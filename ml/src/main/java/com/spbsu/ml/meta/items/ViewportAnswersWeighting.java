7283e9cf46bbf10ecbe7e00e8ccce68612347bfe
Alexander Tolmachev <starlight@yandex-team.ru>
2015-02-12 18:35:41 +0300

    Removed JsonIgnoreProperties annotation for ViewportAnswersWeighting

--- ml/src/main/java/com/spbsu/ml/meta/items/ViewportAnswersWeighting.java
+++ ml/src/main/java/com/spbsu/ml/meta/items/ViewportAnswersWeighting.java
@@ -1,6 +1,5 @@
 package com.spbsu.ml.meta.items;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.spbsu.ml.meta.DSItem;
 
 /**
@@ -8,7 +7,7 @@ import com.spbsu.ml.meta.DSItem;
  * Date: 18.07.14
  * Time: 17:01
  */
-@JsonIgnoreProperties(ignoreUnknown=true)
+//@JsonIgnoreProperties(ignoreUnknown=true)
 public class ViewportAnswersWeighting extends DSItem.Stub implements DSItem {
   public String reqId;
   public String vpName;
