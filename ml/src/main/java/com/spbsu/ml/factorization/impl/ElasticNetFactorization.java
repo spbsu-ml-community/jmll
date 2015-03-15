3a9dc45447daee2749c189ffd2a3e7fc6d04fc56
qdeee <qdeee@yandex-team.ru>
2015-03-16 02:12:10 +0300

    Totally rewrite ElasticNetFactorization: x200 performance

--- ml/src/main/java/com/spbsu/ml/factorization/impl/ElasticNetFactorization.java
+++ ml/src/main/java/com/spbsu/ml/factorization/impl/ElasticNetFactorization.java
@@ -1,22 +1,26 @@
 package com.spbsu.ml.factorization.impl;
 
+import com.spbsu.commons.func.impl.WeakListenerHolderImpl;
 import com.spbsu.commons.math.vectors.*;
 import com.spbsu.commons.math.vectors.impl.iterators.SkipVecNZIterator;
 import com.spbsu.commons.math.vectors.impl.mx.ColsVecArrayMx;
 import com.spbsu.commons.math.vectors.impl.vectors.ArrayVec;
 import com.spbsu.commons.util.Pair;
+import com.spbsu.ml.Trans;
 import com.spbsu.ml.data.set.VecDataSet;
 import com.spbsu.ml.data.set.impl.VecDataSetImpl;
 import com.spbsu.ml.factorization.OuterFactorization;
 import com.spbsu.ml.func.Linear;
 import com.spbsu.ml.loss.L2;
 import com.spbsu.ml.methods.ElasticNetMethod;
+import com.spbsu.ml.methods.ElasticNetMethod2;
+import com.spbsu.ml.methods.VecOptimization;
 
 /**
  * User: qdeee
  * Date: 25.02.15
  */
-public class ElasticNetFactorization implements OuterFactorization {
+public class ElasticNetFactorization extends WeakListenerHolderImpl<Pair<Vec, Vec>> implements OuterFactorization {
   private final int iters;
   private final double tolerance;
   private final double alpha;
@@ -31,72 +35,70 @@ public class ElasticNetFactorization implements OuterFactorization {
 
   @Override
   public Pair<Vec, Vec> factorize(final Mx X) {
-    final ElasticNetMethod elasticNetMethod = new ElasticNetMethod(tolerance, alpha, lambda);
-
-    final int m = X.rows();
-    final int n = X.columns();
-
     final Vec currentU = VecTools.fill(new ArrayVec(X.rows()), 1.0);
     final Vec currentV = VecTools.fill(new ArrayVec(X.columns()), 1.0);
-    final VecDataSet dsForUSearch = new VecDataSetImpl(createMxForDs(currentV, m), null);
-    final VecDataSet dsForVSearch = new VecDataSetImpl(createMxForDs(currentU, n), null);
-    final L2 targetForUSearch = new L2(X, dsForUSearch);
-    final L2 targetForVSearch = new L2(MxTools.transpose(X), dsForVSearch);
+    final Mx transposeX = MxTools.transpose(X);
 
     for (int iter = 0; iter < iters; iter++) {
       //fix v, search u:
-      final Linear modelForU = (Linear) elasticNetMethod.fit(dsForUSearch, targetForUSearch);
-      VecTools.assign(currentU, modelForU.weights);
+      VecTools.assign(currentU, findElasticNetSolution(currentV, X, tolerance, alpha, lambda, 2));
 
       //fix u, search v:
-      final Linear modelForV = (Linear) elasticNetMethod.fit(dsForVSearch, targetForVSearch);
-      VecTools.assign(currentV, modelForV.weights);
+      VecTools.assign(currentV, findElasticNetSolution(currentU, transposeX, tolerance, alpha, lambda, 2));
+
+      invoke(Pair.create(currentU, currentV));
     }
     return Pair.create(currentU, currentV);
   }
 
-  private static Mx createMxForDs(final Vec vecToFix, final int columns) {
-    final Vec[] vecs = new Vec[columns];
+  private static Vec findElasticNetSolution(
+      final Vec vec2fix,
+      final Mx rightSideMx,
+      final double tolerance,
+      final double alpha,
+      final double lambda,
+      final int checkIterations
+  ) {
+    final int columns = rightSideMx.dim() / vec2fix.dim();
+    final double selfFeatureProduct = VecTools.multiply(vec2fix, vec2fix);
+    final double[] gradient = new double[columns];
     for (int j = 0; j < columns; j++) {
-      vecs[j] = arrangeVecInColumn(vecToFix, j * vecToFix.dim(), vecToFix.dim() * columns);
+      gradient[j] = VecTools.multiply(vec2fix, rightSideMx.row(j));
     }
-    return new ColsVecArrayMx(vecs);
+
+    boolean anyUpdated = true;
+    Vec prev = new ArrayVec(columns);
+    Vec betas = new ArrayVec(columns);
+    while (anyUpdated) {
+      anyUpdated = false;
+      VecTools.assign(prev, betas);
+      for (int i = 0; i < checkIterations; i++) {
+        for (int k = 0; k < columns; k++) {
+          final int N = rightSideMx.dim();
+          double newBeta = gradient[k];
+          newBeta = softThreshold(newBeta, N * lambda * alpha);
+          newBeta /= (selfFeatureProduct + N * lambda * (1 - alpha));
+          if (Math.abs(newBeta - betas.get(k)) > 1e-15) {
+            betas.set(k, newBeta);
+            anyUpdated = true;
+          }
+        }
+
+        if (!anyUpdated) {
+          break;
+        }
+      }
+
+      if (VecTools.distance(betas, prev) < tolerance) {
+        break;
+      }
+    }
+
+    return betas;
   }
 
-  private static Vec arrangeVecInColumn(final Vec vecToArrange, final int startPos, final int totalLength) {
-    return new Vec.Stub() {
-      @Override
-      public double get(final int i) {
-        return (i >= startPos && i <= startPos + vecToArrange.dim())
-            ? vecToArrange.get(i % vecToArrange.dim())
-            : 0.;
-      }
-
-      @Override
-      public int dim() {
-        return totalLength;
-      }
-
-      @Override
-      public VecIterator nonZeroes() {
-        return new SkipVecNZIterator(this);
-      }
-
-      @Override
-      public Vec sub(final int start, final int end) {
-        throw new UnsupportedOperationException();
-      }
-
-      @Override
-      public Vec set(final int i, final double val) {
-        throw new UnsupportedOperationException();
-      }
-
-      @Override
-      public Vec adjust(final int i, final double increment) {
-        throw new UnsupportedOperationException();
-      }
-    };
-
+  private static double softThreshold(final double z, final double j) {
+    final double sgn = Math.signum(z);
+    return sgn * Math.max(sgn * z - j, 0);
   }
 }
