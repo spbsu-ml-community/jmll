0d764b15022bcf7027c221802f37dc788db46486
noxoomo <noxoomo@yandex-team.ru>
2015-03-21 19:29:25 +0300

    parallel sort test edition

--- experiments/src/test/java/com/spbsu/exp/BernoulliTest.java
+++ experiments/src/test/java/com/spbsu/exp/BernoulliTest.java
@@ -3,7 +3,8 @@ package com.spbsu.exp;
 import com.spbsu.bernulli.*;
 import com.spbsu.commons.func.Factory;
 import com.spbsu.commons.random.FastRandom;
-import junit.framework.TestCase;
+import com.spbsu.ml.GridTest;
+import org.apache.commons.math3.special.Gamma;
 
 import java.util.Arrays;
 
@@ -12,8 +13,14 @@ import java.util.Arrays;
  * User: noxoomo
  * Date: 09.03.2015
  */
-public class BernoulliTest extends TestCase {
-  FastRandom rand = new FastRandom(42);
+public class BernoulliTest extends GridTest {
+  FastRandom rand;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    rand = new FastRandom(42);
+  }
 
   class BernoulliRand {
     class Result {
@@ -27,43 +34,44 @@ public class BernoulliTest extends TestCase {
         this.means = means;
       }
     }
+
     Result simulate(double[] theta, double[] q, int N, int n) {
       double[] cum = new double[q.length];
       cum[0] = 0;
-      for (int i=1; i < q.length;++i) {
-        cum[i] = cum[i-1] + q[i-1];
+      for (int i = 1; i < q.length; ++i) {
+        cum[i] = cum[i - 1] + q[i - 1];
       }
       double[] sums = new double[N];
       double[] total = new double[N];
       double[] means = new double[N];
       Arrays.fill(total, n);
-      for (int j=0; j < N;++j) {
+      for (int j = 0; j < N; ++j) {
 //        final double coin = rand.nextDouble() ;
 //        final int k = Math.abs(Arrays.binarySearch(cum,coin)) -2;
 //        means[j] = theta[k];
         if (j > 0 && rand.nextDouble() > 0.3) {
           double m = means[rand.nextInt(j)];
-          double s = Math.min(1-m,m) / 8;
-          s *= (1-s);
-          means[j] = m + (2*rand.nextDouble() * s - s);
+          double s = Math.min(1 - m, m) / 8;
+          s *= (1 - s);
+          means[j] = m + (2 * rand.nextDouble() * s - s);
         } else {
           means[j] = rand.nextDouble();
         }
 //        means[j] = 0.1 + rand.nextDouble()*0.8;
-        for (int s = 0; s < n;++s) {
+        for (int s = 0; s < n; ++s) {
           sums[j] += (rand.nextDouble() < means[j]) ? 1.0 : 0;
         }
       }
-      return new Result(sums,total,means);
+      return new Result(sums, total, means);
     }
   }
 
 
   public double l2(double[] a, double[] b) {
-    assert(a.length == b.length);
+    assert (a.length == b.length);
     double sum = 0;
-    for (int i=0; i < a.length;++i) {
-      sum += (a[i] - b[i])*(a[i] - b[i]);
+    for (int i = 0; i < a.length; ++i) {
+      sum += (a[i] - b[i]) * (a[i] - b[i]);
     }
     return sum;
   }
@@ -87,18 +95,18 @@ public class BernoulliTest extends TestCase {
 
   public void testBetaBinomialMixtureEstimation() {
     final int k = 3;
-    int tries = 1000;
+    int tries = 100;
 
     final int from = 100;
-    final int to = 10000;
-    final int step = 20;
+    final int to = 100000;
+    final int step = 1000;
 
-    for (int n = 100; n < 110; n += 10)
+    for (int n = 100; n < 10000; n += 1000)
       for (int N = from; N < to; N += step) {
         double sumAvgMixture = 0;
         double sumAvgNaive = 0;
         for (int tr = 0; tr < tries; ++tr) {
-          BetaBinomialMixture mix = new BetaBinomialMixture(3, rand);
+          BetaBinomialMixture mix = new BetaBinomialMixture(5, rand);
           final BetaBinomialMixture.Observations observations = mix.next(N, n);
           final int finaln = n;
           StochasticSearch<BetaBinomialMixtureEM> search = new StochasticSearch<>(new Factory<Learner<BetaBinomialMixtureEM>>() {
@@ -114,7 +122,7 @@ public class BernoulliTest extends TestCase {
               };
             }
           });
-          BetaBinomialMixtureEM em = search.fit(10);
+          BetaBinomialMixtureEM em = search.fit(8);
 //          BetaBinomialMixtureEM em = new BetaBinomialMixtureEM(k,observations.sums,n,rand);
 
           double[] means = em.estimate(false);
@@ -130,9 +138,9 @@ public class BernoulliTest extends TestCase {
   }
 
 
-  public void testBetaFunctionsProprtion() {
+  public void testSpecialFunctionCache() {
 //    BetaFunctionsProportion prop = new  BetaFunctionsProportion(0.5,0.5,50);
-    BetaFunctionsProportion prop = new BetaFunctionsProportion(1, 1, 10);
+    SpecialFunctionCache prop = new SpecialFunctionCache(1, 1, 10);
 //    assertTrue((prop.calculate(10,50)-1.860013246596769 * 1e-13) < 1e-20);
 //    prop.update(1,1);
     assertTrue(Math.abs(prop.calculate(2, 10) - 0.0020202) < 1e-7);
@@ -142,62 +150,82 @@ public class BernoulliTest extends TestCase {
     assertTrue(Math.abs(prop.calculate(9, 10) - 0.00909091) < 1e-7);
     assertTrue(Math.abs(prop.calculate(10, 10) - 0.0909091) < 1e-7);
 
+    for (int i = 0; i < 10; ++i) {
+      assertTrue(Math.abs(prop.digamma(SpecialFunctionCache.Type.Alpha, i) - Gamma.digamma(1 + i)) < 1e-12);
+      assertTrue(Math.abs(prop.digamma(SpecialFunctionCache.Type.Beta, i) - Gamma.digamma(1 + i)) < 1e-12);
+      assertTrue(Math.abs(prop.digamma(SpecialFunctionCache.Type.AlphaBeta, i) - Gamma.digamma(2 + i)) < 1e-12);
+    }
 
     prop.update(102.5, 10.11);
 //    prop = new  BetaFunctionsProportion(102.5,10.11,10);
     assertTrue(Math.abs(prop.calculate(5, 10) - 6.478713223568241e-6) < 1e-9);
     assertTrue(Math.abs(prop.calculate(8, 10) - 0.00369375) < 1e-8);
 
-    prop = new BetaFunctionsProportion(12.2, 55.1, 100);
+    for (int i = 0; i < 10; ++i) {
+      assertTrue(Math.abs(prop.digamma(SpecialFunctionCache.Type.Alpha, i) - Gamma.digamma(102.5 + i)) < 1e-12);
+      assertTrue(Math.abs(prop.digamma(SpecialFunctionCache.Type.Beta, i) - Gamma.digamma(10.11 + i)) < 1e-12);
+      assertTrue(Math.abs(prop.digamma(SpecialFunctionCache.Type.AlphaBeta, i) - Gamma.digamma(102.5 + 10.11 + i)) < 1e-12);
+    }
+
+    prop = new SpecialFunctionCache(12.2, 55.1, 100);
 
     assertTrue(Math.abs(prop.calculate(30, 100) - 3.520721627628687e-28) < 1e-32);
     assertTrue(Math.abs(prop.calculate(60, 100) - 7.007620723590574e-37) < 1e-41);
     assertTrue(Math.abs(prop.calculate(10, 100) - 1.764175281317258e-15) < 1e-19);
     assertTrue(Math.abs(prop.calculate(88, 100) - 3.97033387162681e-36) < 1e-40);
+
+
+    for (int i = 0; i < 100; ++i) {
+      assertTrue(Math.abs(prop.digamma(SpecialFunctionCache.Type.Alpha, i) - Gamma.digamma(12.2 + i)) < 1e-12);
+      assertTrue(Math.abs(prop.digamma(SpecialFunctionCache.Type.Beta, i) - Gamma.digamma(55.1 + i)) < 1e-12);
+      assertTrue(Math.abs(prop.digamma(SpecialFunctionCache.Type.AlphaBeta, i) - Gamma.digamma(12.2 + 55.1 + i)) < 1e-12);
+    }
   }
+
   public void testMixture() {
-    final int k = 50;
-    final int from = 500;
-    final int to = 1000;
-    final int step = 20;
+    final int k = 20;
+    final int from = 100;
+    final int to = 200;
+    final int step = 50;
     final int tries = 1000;
     BernoulliRand brand = new BernoulliRand();
 
 
     double[] theta = new double[k];
     double[] q = new double[k];
-    for (int n = 50; n < 110; n += 10)
-    for (int N = from; N < to; N += step) {
-      double sumAvgMixture = 0;
-      double sumAvgNaive = 0;
-      for (int tr = 0; tr < tries;++tr) {
-        {
-          double totalWeight = 0;
-          for (int i = 0; i < q.length; ++i) {
-            q[i] = rand.nextDouble();
-            totalWeight += q[i];
+    for (int n = 50; n < 70; n += 10)
+      for (int N = from; N < to; N += step) {
+        double sumAvgMixture = 0;
+        double sumAvgNaive = 0;
+        for (int tr = 0; tr < tries; ++tr) {
+          {
+            double totalWeight = 0;
+            for (int i = 0; i < q.length; ++i) {
+              q[i] = rand.nextDouble();
+              totalWeight += q[i];
+            }
+            for (int i = 0; i < q.length; ++i) {
+              q[i] /= totalWeight;
+            }
           }
-          for (int i = 0; i < q.length; ++i) {
-            q[i] /= totalWeight;
+          {
+            for (int i = 0; i < theta.length; ++i) {
+              theta[i] = rand.nextDouble() / 4;
+            }
           }
-        }
-        {
-          for (int i = 0; i < theta.length; ++i) {
-            theta[i] = rand.nextDouble() / 4;
+          BernoulliRand.Result experiment = brand.simulate(theta, q, N, n);
+          BernoulliMixture em = new BernoulliMixture();
+          double[] means = em.estimate(experiment.sums, experiment.total, k);
+          double[] naive = experiment.sums.clone();
+          for (int i = 0; i < naive.length; ++i) {
+            naive[i] /= experiment.total[i];
           }
+          sumAvgMixture += l2(means, experiment.means) / experiment.means.length;
+          sumAvgNaive += l2(naive, experiment.means) / experiment.means.length;
         }
-        BernoulliRand.Result experiment = brand.simulate(theta, q, N, n);
-        BernoulliMixture em = new BernoulliMixture();
-        double[] means = em.estimate(experiment.sums, experiment.total, k);
-        double[] naive = experiment.sums.clone();
-        for (int i=0; i < naive.length;++i) {
-          naive[i] /= experiment.total[i];
-        }
-        sumAvgMixture += l2(means, experiment.means) / experiment.means.length;
-        sumAvgNaive += l2(naive, experiment.means) / experiment.means.length;
+        System.out.println(N + "\t" + n + "\t" + sumAvgMixture / tries + "\t" + sumAvgNaive / tries);
       }
-      System.out.println(N + "\t" + n +  "\t" + sumAvgMixture / tries + "\t" + sumAvgNaive / tries);
-    }
   }
+
 
 }
