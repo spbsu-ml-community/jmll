8cbcfdd0194629825cdcd05f21409007197908b5
qdeee <qdeee@yandex-team.ru>
2015-03-25 22:55:59 +0300

    added gridsearch mode for cli

--- ml/src/main/java/com/spbsu/ml/cli/JMLLCLI.java
+++ ml/src/main/java/com/spbsu/ml/cli/JMLLCLI.java
@@ -16,6 +16,8 @@ import com.spbsu.ml.cli.builders.data.impl.DataBuilderCrossValidation;
 import com.spbsu.ml.cli.builders.methods.MethodsBuilder;
 import com.spbsu.ml.cli.builders.methods.grid.DynamicGridBuilder;
 import com.spbsu.ml.cli.builders.methods.grid.GridBuilder;
+import com.spbsu.ml.cli.gridsearch.GridSearch;
+import com.spbsu.ml.cli.gridsearch.ParametersExtractor;
 import com.spbsu.ml.cli.output.ModelWriter;
 import com.spbsu.ml.cli.output.printers.DefaultProgressPrinter;
 import com.spbsu.ml.cli.output.printers.HistogramPrinter;
@@ -66,7 +68,7 @@ public class JMLLCLI {
 
   private static final String MODEL_OPTION = "m";
 
-  public static final String RANGES = "r";
+  public static final String RANGES_OPTION = "r";
 
   static Options options = new Options();
 
@@ -94,7 +96,7 @@ public class JMLLCLI {
 
     options.addOption(OptionBuilder.withLongOpt("model").withDescription("model file").hasArg().create(MODEL_OPTION));
 
-    options.addOption(OptionBuilder.withLongOpt("ranges").withLongOpt("parameters ranges").hasArg().create(RANGES));
+    options.addOption(OptionBuilder.withLongOpt("ranges").withLongOpt("parameters ranges").hasArg().create(RANGES_OPTION));
   }
 
   public static void main(final String[] args) throws IOException {
@@ -115,7 +117,7 @@ public class JMLLCLI {
         case "validate-pool":             modeValidatePool(command);            break;
         case "split-json-pool":           modeSplitJsonPool(command);           break;
         case "print-pool-info":           modePrintPoolInfo(command);           break;
-        case "gridSearch":                modeGridSearch(command);              break;
+        case "grid-search":                modeGridSearch(command);             break;
         default:
           throw new RuntimeException("Mode " + mode + " is not recognized");
       }
@@ -395,8 +397,73 @@ public class JMLLCLI {
     System.out.println(DataTools.getPoolInfo(pool));
   }
 
-  private static void modeGridSearch(final CommandLine command) {
+  private static void modeGridSearch(final CommandLine command) throws MissingArgumentException, IOException {
+    if (!command.hasOption(LEARN_OPTION)) {
+      throw new MissingArgumentException("Please provide 'LEARN_OPTION'");
+    }
+    if (!command.hasOption(OPTIMIZATION_OPTION)) {
+      throw new MissingArgumentException("Please provide 'OPTIMIZATION_OPTION'");
+    }
+    if (!command.hasOption(RANGES_OPTION)) {
+      throw new MissingArgumentException("Please provide 'RANGE_OPTION'");
+    }
 
+    //data loading
+    final DataBuilder dataBuilder;
+    if (command.hasOption(CROSS_VALIDATION_OPTION)) {
+      final DataBuilderCrossValidation dataBuilderCrossValidation = new DataBuilderCrossValidation();
+      final String[] cvOptions = StringUtils.split(command.getOptionValue(CROSS_VALIDATION_OPTION), "/", 2);
+      dataBuilderCrossValidation.setRandomSeed(Integer.valueOf(cvOptions[0]));
+      dataBuilderCrossValidation.setPartition(Double.valueOf(cvOptions[1]));
+      dataBuilder = dataBuilderCrossValidation;
+    } else {
+      dataBuilder = new DataBuilderClassic();
+      ((DataBuilderClassic) dataBuilder).setTestPath(command.getOptionValue(TEST_OPTION));
+    }
+    dataBuilder.setLearnPath(command.getOptionValue(LEARN_OPTION));
+    dataBuilder.setJsonFormat(command.hasOption(JSON_FORMAT));
+
+    final Pair<? extends Pool, ? extends Pool> pools = dataBuilder.create();
+    final Pool learn = pools.getFirst();
+    final Pool test = pools.getSecond();
+
+    //loading grid (if needed)
+    final GridBuilder gridBuilder = new GridBuilder();
+    if (command.hasOption(GRID_OPTION)) {
+      gridBuilder.setGrid(BFGrid.CONVERTER.convertFrom(StreamTools.readFile(new File(command.getOptionValue(GRID_OPTION)))));
+    } else {
+      gridBuilder.setBinsCount(Integer.valueOf(command.getOptionValue(BIN_FOLDS_COUNT_OPTION, "32")));
+      gridBuilder.setDataSet(learn.vecData());
+    }
+
+    //choose optimization method
+    final MethodsBuilder methodsBuilder = new MethodsBuilder();
+    methodsBuilder.setRandom(new FastRandom());
+    methodsBuilder.setGridBuilder(gridBuilder);
+
+    //set target
+    final String target = command.getOptionValue(TARGET_OPTION, DEFAULT_TARGET);
+    final TargetFunc loss = learn.target(DataTools.targetByName(target));
+
+    //set metrics
+    final String[] metricNames = command.getOptionValues(METRICS_OPTION);
+    final Func[] metrics;
+    if (metricNames != null) {
+      metrics = new Func[metricNames.length];
+      for (int i = 0; i < metricNames.length; i++) {
+        metrics[i] = test.target(DataTools.targetByName(metricNames[i]));
+      }
+    } else {
+      metrics = new Func[]{test.target(DataTools.targetByName(target))};
+    }
+
+    final GridSearch gridSearch = new GridSearch(learn, test, loss, metrics, methodsBuilder);
+    final String commonScheme = command.getOptionValue(OPTIMIZATION_OPTION);
+    final String[][] parametersSpace = ParametersExtractor.parse(command.getOptionValue(RANGES_OPTION));
+    final GridSearch.OptimumHolder[] searchResult = gridSearch.search(commonScheme, parametersSpace);
+    for (int i = 0; i < metrics.length; i++) {
+      System.out.println(metrics[i].getClass().getSimpleName() + " : " + searchResult[i]);
+    }
   }
 
   private static String getOutputName(final CommandLine command) {
