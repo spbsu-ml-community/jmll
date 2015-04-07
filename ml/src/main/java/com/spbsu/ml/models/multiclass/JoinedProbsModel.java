4b888b33223d4ec5310a19715af317a6ba634db7
qdeee <qdeee@yandex-team.ru>
2015-04-07 16:55:20 +0300

    Added JoinedProbsModel, serialization, test

---
+++ ml/src/main/java/com/spbsu/ml/models/multiclass/JoinedProbsModel.java
@@ -0,0 +1,25 @@
+package com.spbsu.ml.models.multiclass;
+
+import com.spbsu.commons.math.MathTools;
+import com.spbsu.commons.math.vectors.Vec;
+import com.spbsu.ml.Func;
+import com.spbsu.ml.func.FuncJoin;
+
+/**
+ * User: qdeee
+ * Date: 07.04.15
+ */
+public class JoinedProbsModel extends FuncJoin {
+  public JoinedProbsModel(final Func[] dirs) {
+    super(dirs);
+  }
+
+  @Override
+  public Vec trans(final Vec x) {
+    final Vec result = super.trans(x);
+    for (int i = 0; i < result.dim(); i++) {
+      result.set(i, MathTools.sigmoid(result.get(i)));
+    }
+    return result;
+  }
+}
