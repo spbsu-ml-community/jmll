d8d992a7e67edb8a27a1654922c9d6fefef3c86a
qdeee <qdeee@yandex-team.ru>
2015-09-17 19:00:27 +0300

    repair gradfac after Trans refactoring

--- ml/src/main/java/com/spbsu/ml/methods/multiclass/gradfac/GradFacSvdNMulticlass.java
+++ ml/src/main/java/com/spbsu/ml/methods/multiclass/gradfac/GradFacSvdNMulticlass.java
@@ -4,6 +4,7 @@ import com.spbsu.commons.math.vectors.Mx;
 import com.spbsu.commons.math.vectors.MxTools;
 import com.spbsu.commons.math.vectors.Vec;
 import com.spbsu.commons.math.vectors.VecTools;
+import com.spbsu.commons.math.vectors.impl.mx.VecBasedMx;
 import com.spbsu.commons.util.Pair;
 import com.spbsu.ml.Func;
 import com.spbsu.ml.data.set.VecDataSet;
@@ -41,7 +42,10 @@ public class GradFacSvdNMulticlass implements VecOptimization<L2> {
 
   @Override
   public FuncJoin fit(VecDataSet learn, L2 mllLogitGradient) {
-    final Mx gradient = (Mx)mllLogitGradient.target;
+    final Mx gradient = mllLogitGradient.target instanceof Mx
+        ? (Mx)mllLogitGradient.target
+        : new VecBasedMx(mllLogitGradient.target.dim() / learn.length(), mllLogitGradient.target);
+
     final Pair<Vec, Vec> pair = outerFactorization.factorize(gradient);
 
     final Mx h = (Mx) pair.getFirst();
