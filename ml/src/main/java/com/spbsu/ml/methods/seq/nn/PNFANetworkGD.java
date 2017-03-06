e04bd5fec70279dc2bcf59a83f5c855b173bbd84
solar <ikuralenok@gmail.com>
2017-03-06 14:10:29 +0300

    Times studies on PNFA 6.03.2017

--- ml/src/main/java/com/spbsu/ml/methods/seq/nn/PNFANetworkGD.java
+++ ml/src/main/java/com/spbsu/ml/methods/seq/nn/PNFANetworkGD.java
@@ -9,7 +9,6 @@ import com.spbsu.commons.math.vectors.impl.vectors.ArrayVec;
 import com.spbsu.commons.random.FastRandom;
 import com.spbsu.commons.seq.Seq;
 import com.spbsu.commons.seq.regexp.Alphabet;
-import com.spbsu.commons.util.ArrayTools;
 import com.spbsu.ml.data.set.DataSet;
 import com.spbsu.ml.loss.L2;
 import com.spbsu.ml.methods.SeqOptimization;
@@ -19,7 +18,9 @@ import gnu.trove.list.array.TIntArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static com.spbsu.commons.math.vectors.VecTools.*;
+import static com.spbsu.commons.math.vectors.VecTools.append;
+import static com.spbsu.commons.math.vectors.VecTools.fill;
+import static com.spbsu.commons.math.vectors.VecTools.sum;
 
 public class PNFANetworkGD<T, Loss extends L2> implements SeqOptimization<T, Loss> {
   private final int stateCount;
@@ -46,95 +47,152 @@ public class PNFANetworkGD<T, Loss extends L2> implements SeqOptimization<T, Los
   @Override
   public Computable<Seq<T>, Vec> fit(DataSet<Seq<T>> learn, Loss loss) {
     final PNFAParams<T> params = new PNFAParams<>(random, stateCount, alphabet);
-    final Vec[] distribs = new Vec[learn.length()];
-    for (int i = 0; i < distribs.length; i++) {
-      distribs[i] = new ArrayVec(stateCount);
-      fill(distribs[i], 1./distribs[i].dim());
+    final PNFAParams<T>[] taskParams = new PNFAParams[threadCount];
+
+    int maxLen = 0;
+    for (int i = 0; i < learn.length(); i++) {
+      maxLen = Math.max(maxLen, learn.at(i).length());
     }
 
-    long start = System.nanoTime();
-    final Mx[] bGradients = new Mx[alphabet.size()];
-    final Vec vGradients = new ArrayVec(stateCount);
-
-    for (int i = 0; i < bGradients.length; i++) {
-      bGradients[i] = new VecBasedMx(stateCount, stateCount - 1);
+//    final int[] path = new int[maxLen + 1];
+    for (int i = 0; i < threadCount; i++) {
+      taskParams[i] = new PNFAParams<>(random, stateCount, alphabet);
     }
-    params.updateParams(bGradients, vGradients, learn, loss.target(), distribs, 0);
+
+    final ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+
+//    final Mx[] totalWGrad = new Mx[alphabet.size()];
+//    final Vec totalValuesGrad = new ArrayVec(stateCount);
+//    for (int i = 0; i < alphabet.size(); i++) {
+//      totalWGrad[i] = new VecBasedMx(stateCount, stateCount);
+//    }
+
     double curCost = getCost(learn, loss, params);
-
-    final int[] path = new int[4000];
-    final double[] transitions = new double[4000];
-
+    long start = System.nanoTime();
+    final Trans gradient = loss.gradient();
+    final Mx[] wGradients = new Mx[alphabet.size()];
+    for (int i = 0; i < wGradients.length; i++) {
+      wGradients[i] = new VecBasedMx(stateCount, stateCount);
+    }
+    final Vec vGradients = new ArrayVec(stateCount);
     for (int iter = 0; iter < iterationsCount; iter++) {
-      fill(vGradients, 0);
-      for (final Mx bGradient : bGradients) {
-        fill(bGradient, 0);
+//      for (int i = 0; i < alphabet.size(); i++) {
+//        fill(totalWGrad[i], 0);
+//      }
+//      fill(totalValuesGrad, 0);
+      for (final Mx wGradient : wGradients) {
+        fill(wGradient, 0);
       }
+      fill(vGradients, 0);
 
-      final int samplesCount = learn.length();
-      for (int i = 0; i < samplesCount; i++) {
+      for (int i = 0; i < loss.dim(); i++) {
         final int sampleIdx = random.nextInt(learn.length());
         final Seq<T> sample = learn.at(sampleIdx);
         int currentState = 0;
-        //path.clear();
-        //transitions.clear();
+        final TIntArrayList path = new TIntArrayList(sample.length());
+        final TDoubleArrayList transitions = new TDoubleArrayList(sample.length());
         for (int t = 0; t < sample.length(); t++) {
           final Vec weights = params.getW()[alphabet.index(sample.at(t))].row(currentState);
-          currentState = random.nextSimple(weights);
-          path[t] = currentState;
-          transitions[t] = weights.get(currentState);
+          path.add(currentState = random.nextSimple(weights));
+          transitions.add(weights.get(currentState));
         }
-        final double finalStateValue = params.getValues().get(currentState);
-        final double pLastState = distribs[sampleIdx].get(currentState);
-        double residual = 2 * (loss.target().get(sampleIdx) - multiply(distribs[sampleIdx], params.getValues()));
-        for (int k = 0; k < vGradients.length(); k++) {
-          vGradients.adjust(k, residual*distribs[sampleIdx].get(k));
-        }
-        residual *= finalStateValue;
+        double finalStateValue = params.getValues().get(currentState);
+        double residual = 2 * (loss.target().get(sampleIdx) - params.getSeqValue(sample)) * finalStateValue;
+        vGradients.adjust(currentState, residual / finalStateValue);
         for (int t = sample.length() - 1; t >= 0; t--) {
-          currentState = path[t];
-          final int prevState = t > 0 ? path[t - 1] : 0;
-          final double prevProb = t > 0 ? transitions[t - 1] : 1;
+          currentState = path.get(t);
+          final int prevState = t > 0 ? path.get(t - 1) : 0;
           final Vec weights = params.getW()[alphabet.index(sample.at(t))].row(prevState);
-          final Vec vec = bGradients[alphabet.index(sample.at(t))].row(prevState);
-          final double rresidual = residual / weights.get(currentState);
-
-          for (int j = 0; j < stateCount - 1; j++) {
-            final double w = weights.get(j);
-            if (j == currentState) {
-              vec.adjust(j, rresidual * w * (1 - w));
-            }
-            else {
-              vec.adjust(j, -rresidual * w * weights.get(currentState));
-            }
+          final Vec vec = wGradients[alphabet.index(sample.at(t))].row(prevState);
+          final double sum = 1 + sum(weights);
+          for (int j = 0; j < vec.dim(); j++) {
+            vec.adjust(j, residual * (sum - weights.get(j))/sum/sum/ weights.get(currentState));
           }
         }
       }
-
-      if (iter % 500 < 10 || iter % 500 > 460) {
-        for (final Mx bGradient : bGradients) {
-          fill(bGradient, 0);
-        }
-      }
-      else fill(vGradients, 0);
-
       // step
-      params.updateParams(bGradients, vGradients, learn, loss.target(), distribs, step/samplesCount);
-      double totalGrad = 0;
-      for (int i = 0; i < bGradients.length; i++) {
-        totalGrad += VecTools.norm(bGradients[i]);
-      }
+      params.updateParams(wGradients, vGradients, step/loss.dim());
+
+//      final List<Future<Vec>> pathFutures = new ArrayList<>(threadCount);
+//      List<Seq<T>> curSeqs = new ArrayList<>(learn.length());
+//      for (int i = 0; i < learn.length(); i++) {
+//        curSeqs.add(learn.at(i));
+//      }
+//
+//      path[0] = 0;
+//      for (int posInSeq = 0; posInSeq < maxLen; posInSeq++) {
+//        pathFutures.clear();
+//        final int posInSeq1 = posInSeq;
+//        curSeqs = curSeqs.stream().filter(seq -> posInSeq1 < seq.length()).collect(Collectors.toList());
+//        for (int taskId = 0; taskId < threadCount; taskId++) {
+//          final int taskId1 = taskId;
+//          final int samplesPerThread = (curSeqs.size() + threadCount - 1) / threadCount;
+//          final int startSub = samplesPerThread * taskId1;
+//          final int endSub = Math.min(curSeqs.size()  , (taskId1 + 1) * samplesPerThread);
+//          final List<Seq<T>> curSeqs1 = curSeqs;
+//
+//          taskParams[taskId1].setParams(params);
+//          pathFutures.add(executorService.submit(() -> taskParams[taskId1].getNextStateDistribution(
+//                  path[posInSeq1], posInSeq1, curSeqs1, startSub, endSub
+//          )));
+//        }
+//
+//        final Vec distribution = new ArrayVec(stateCount);
+//        pathFutures.forEach(future -> {
+//          try {
+//            VecTools.append(distribution, future.get());
+//          } catch (InterruptedException | ExecutionException e) {
+//            e.printStackTrace();
+//          }
+//        });
+//
+//        VecTools.normalizeL1(distribution);
+//        double rnd = random.nextDouble();
+//        path[posInSeq + 1] = stateCount - 1;
+//        for (int to = 0; to < stateCount; to++) {
+//          if (rnd <= distribution.at(to)) {
+//            path[posInSeq + 1] = to;
+//            break;
+//          }
+//          rnd -= distribution.at(to);
+//        }
+//      }
+//
+//      final List<Future<PNFAParams.PNFAParamsGrad>> gradFutures = new ArrayList<>(threadCount);
+//
+//      for (int taskId = 0; taskId < threadCount; taskId++) {
+//        taskParams[taskId].setParams(params);
+//        final int samplesPerThread = (learn.length() + threadCount - 1) / threadCount;
+//        final int taskId1 = taskId;
+//        final int startSub = samplesPerThread * taskId;
+//        final int endSub = Math.min(learn.length(), (taskId + 1) * samplesPerThread);
+//
+//        gradFutures.add(executorService.submit(() -> taskParams[taskId1].calcPathGrad(
+//                learn.sub(startSub, endSub),
+//                loss.target().sub(startSub, endSub - startSub),
+//                path)
+//        ));
+//      }
+//
+//      gradFutures.forEach(future -> {
+//        try {
+//          final PNFAParams.PNFAParamsGrad grad = future.get();
+//          for (int i = 0; i < alphabet.size(); i++) {
+//            VecTools.append(totalWGrad[i], grad.getWGrad()[i]);
+//          }
+//          VecTools.append(totalValuesGrad, grad.getValuesGrad());
+//        } catch (InterruptedException | ExecutionException e) {
+//          e.printStackTrace();
+//        }
+//      });
+//
+//      params.updateParams(totalWGrad, totalValuesGrad, -step / learn.length());
+
       if (iter % 500 == 0) {
         long cur = System.nanoTime();
-        double newCost = 0;
-        for (int i = 0; i < learn.length(); i++) {
-          final double sqr = MathTools.sqr(multiply(distribs[i], params.getValues()) - loss.target().at(i));
-          newCost += sqr;
-        }
-        newCost /= learn.length();
-
+        final double newCost = getCost(learn, loss, params);
         //System.out.printf("Iteration %d, cost=%.6f\n", iter, newCost);
-        System.out.printf("Iterations elapsed  %d, cost=%.6f, 100 iterations passed in %.2f minutes\n", iter, curCost, (cur - start) / 60e9);
+        System.out.printf("Iterations elapsed  %d, cost=%.6f, 500 iterations passed in %.2f minutes\n", iter, curCost, (cur - start) / 60e9);
         System.out.flush();
         start = cur;
         if (newCost > curCost) {
@@ -142,7 +200,18 @@ public class PNFANetworkGD<T, Loss extends L2> implements SeqOptimization<T, Los
           System.out.println("Values: " + params.getValues());
           Vec weights = new ArrayVec(stateCount);
           for (int i = 0; i < loss.dim(); i++) {
-            append(weights, distribs[i]);
+            Mx distribution = new VecBasedMx(1, stateCount), distribution1 = new VecBasedMx(1, stateCount);
+            Seq<T> seq = learn.at(i);
+            distribution.set(0, 1);
+            for (int j = 0; j < seq.length(); j++) {
+              VecTools.fill(distribution1, 0);
+              MxTools.multiplyTo(distribution, params.getW()[alphabet.index(seq.at(j))], distribution1);
+              final Mx tmp = distribution;
+              distribution = distribution1;
+              distribution1 = tmp;
+            }
+
+            append(weights, distribution);
           }
           System.out.println("Weights: " + weights);
           break;
@@ -150,14 +219,14 @@ public class PNFANetworkGD<T, Loss extends L2> implements SeqOptimization<T, Los
         curCost= newCost;
       }
     }
+
     return (seq) -> new SingleValueVec(params.getSeqValue(seq));
   }
 
   private double getCost(final DataSet<Seq<T>> learn, final Loss loss, final PNFAParams<T> params) {
     double cost = 0;
     for (int i = 0; i < learn.length(); i++) {
-      final double sqr = MathTools.sqr(params.getSeqValue(learn.at(i)) - loss.target().at(i));
-      cost += sqr;
+      cost += MathTools.sqr(params.getSeqValue(learn.at(i)) - loss.target().at(i));
     }
     return cost / learn.length();
   }
