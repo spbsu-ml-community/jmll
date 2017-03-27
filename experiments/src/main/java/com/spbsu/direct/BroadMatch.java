dd10d62d4719421e7a5681415922e441c524090c
Yurii Rebryk <y.a.rebryk@gmail.com>
2017-03-27 23:40:27 +0300

    Move util functions to the separate class

--- experiments/src/main/java/com/spbsu/direct/BroadMatch.java
+++ experiments/src/main/java/com/spbsu/direct/BroadMatch.java
@@ -11,18 +11,13 @@ import com.spbsu.commons.util.ThreadTools;
 import com.spbsu.direct.gen.SimpleGenerativeModel;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
-import gnu.trove.map.TObjectDoubleMap;
-import gnu.trove.map.custom_hash.TObjectDoubleCustomHashMap;
-import gnu.trove.strategy.HashingStrategy;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import static com.spbsu.direct.Utils.*;
 
 /**
  * User: solar
@@ -264,69 +259,5 @@ public class BroadMatch {
         });
       }
     }
-  }
-
-  @Nullable
-  private static String normalizeQuery(String query) {
-    query = query.replaceAll("[;,.:\\(\\)\"\'«»!\\]\\[\\{\\}<>]", "");
-    query = query.replaceAll("\\s+", " ");
-    return query.toLowerCase();
-  }
-
-  @NotNull
-  private static ListDictionary<CharSeq> loadDictionaryWithFreqs(String arg, final TIntList freqs) throws IOException {
-    final ListDictionary<CharSeq> dict;
-    { // dict
-      final TObjectDoubleMap<Seq<CharSeq>> freqsHash = new TObjectDoubleCustomHashMap<>(new HashingStrategy<Object>() {
-        @Override
-        public int computeHashCode(Object object) {
-          return object.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object o1, Object o2) {
-          return o1.equals(o2);
-        }
-      });
-      final List<Seq<CharSeq>> dictSeqs = new ArrayList<>();
-
-      CharSeqTools.processLines(StreamTools.openTextFile(arg), (Action<CharSequence>) line -> {
-        final CharSequence[] split = CharSeqTools.split(line, '\t', new CharSequence[2]);
-        final CharSequence[] parts = CharSeqTools.split(split[0].subSequence(1, split[0].length() - 1), ", ");
-        final SeqBuilder<CharSeq> builder = new ArraySeqBuilder<>(CharSeq.class);
-        for (final CharSequence part : parts) {
-          builder.add(CharSeq.create(part.toString()));
-        }
-        final Seq<CharSeq> seq = builder.build();
-        dictSeqs.add(seq);
-        freqsHash.put(seq, CharSeqTools.parseDouble(split[1]));
-      });
-      //noinspection unchecked
-      dict = new ListDictionary<>(dictSeqs.toArray(new Seq[dictSeqs.size()]));
-      freqs.fill(0, dict.size(), 0);
-      for (int i = 0; i < dict.size(); i++) {
-        final double val = freqsHash.get(dict.get(i));
-        freqs.add((int)val);
-      }
-    }
-    return dict;
-  }
-
-  @Nullable
-  private static IntSeq dropUnknown(IntSeq parse) {
-    final IntSeqBuilder builder = new IntSeqBuilder();
-    for (int i = 0; i < parse.length(); i++) {
-      final int val = parse.intAt(i);
-      if (val >= 0)
-        builder.add(val);
-    }
-    return builder.length() > 0 ? builder.build() : null;
-  }
-
-  private static ArraySeq<CharSeq> convertToSeq(CharSequence word) {
-    final CharSeq[] words = new CharSeq[100];
-    final String query = word.toString();
-    final int wcount = CharSeqTools.trySplit(CharSeq.create(query), ' ', words);
-    return new ArraySeq<>(words, 0, wcount);
   }
 }
---
+++ experiments/src/main/java/com/spbsu/direct/Utils.java
@@ -0,0 +1,86 @@
+package com.spbsu.direct;
+
+import com.spbsu.commons.func.Action;
+import com.spbsu.commons.io.StreamTools;
+import com.spbsu.commons.io.codec.seq.ListDictionary;
+import com.spbsu.commons.seq.*;
+import gnu.trove.list.TIntList;
+import gnu.trove.map.TObjectDoubleMap;
+import gnu.trove.map.custom_hash.TObjectDoubleCustomHashMap;
+import gnu.trove.strategy.HashingStrategy;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public final class Utils {
+  private static final int SEQ_SIZE = 100;
+
+  @Nullable
+  public static IntSeq dropUnknown(IntSeq parse) {
+    final IntSeqBuilder builder = new IntSeqBuilder();
+    Arrays.stream(parse.arr).filter(val -> val >= 0).forEach(builder::add);
+    return builder.length() > 0 ? builder.build() : null;
+  }
+
+  @NotNull
+  public static ArraySeq<CharSeq> convertToSeq(CharSequence word) {
+    final CharSeq[] words = new CharSeq[SEQ_SIZE];
+    final int wordsCount = CharSeqTools.trySplit(CharSeq.create(word.toString()), ' ', words);
+    return new ArraySeq<>(words, 0, wordsCount);
+  }
+
+  @Nullable
+  public static String normalizeQuery(String query) {
+    query = query.replaceAll("[;,.:\\(\\)\"\'«»!\\]\\[\\{\\}<>]", "");
+    query = query.replaceAll("\\s+", " ");
+    return query.toLowerCase();
+  }
+
+  @NotNull
+  public static ListDictionary<CharSeq> loadDictionaryWithFreqs(String arg, final TIntList freqs) throws IOException {
+    final ListDictionary<CharSeq> dict;
+
+    final TObjectDoubleMap<Seq<CharSeq>> freqsHash = new TObjectDoubleCustomHashMap<>(new HashingStrategy<Object>() {
+      @Override
+      public int computeHashCode(Object object) {
+        return object.hashCode();
+      }
+
+      @Override
+      public boolean equals(Object o1, Object o2) {
+        return o1.equals(o2);
+      }
+    });
+
+    final List<Seq<CharSeq>> dictSeqs = new ArrayList<>();
+
+    CharSeqTools.processLines(StreamTools.openTextFile(arg), (Action<CharSequence>) line -> {
+      final CharSequence[] split = CharSeqTools.split(line, '\t', new CharSequence[2]);
+      final CharSequence[] parts = CharSeqTools.split(split[0].subSequence(1, split[0].length() - 1), ", ");
+      final SeqBuilder<CharSeq> builder = new ArraySeqBuilder<>(CharSeq.class);
+
+      for (final CharSequence part : parts) {
+        builder.add(CharSeq.create(part.toString()));
+      }
+
+      final Seq<CharSeq> seq = builder.build();
+      dictSeqs.add(seq);
+      freqsHash.put(seq, CharSeqTools.parseDouble(split[1]));
+    });
+
+    //noinspection unchecked
+    dict = new ListDictionary<>(dictSeqs.toArray(new Seq[dictSeqs.size()]));
+
+    freqs.fill(0, dict.size(), 0);
+    for (int i = 0; i < dict.size(); i++) {
+      freqs.add((int) freqsHash.get(dict.get(i)));
+    }
+
+    return dict;
+  }
+}
