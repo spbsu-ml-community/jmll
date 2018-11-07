f2cf39451054ed12593a3926e040d1e107c60247
Yurii Rebryk <y.a.rebryk@gmail.com>
2018-11-07 19:15:52 +0300

    Add opportunity to dump the FMCBoosting model

--- experiments/src/main/java/com/expleague/fmcb/FMCBoostingMain.java
+++ experiments/src/main/java/com/expleague/fmcb/FMCBoostingMain.java
@@ -1,6 +1,7 @@
 package com.expleague.fmcb;
 
 import com.expleague.commons.math.Trans;
+import com.expleague.commons.math.vectors.Vec;
 import com.expleague.commons.random.FastRandom;
 import com.expleague.commons.util.logging.Interval;
 import com.expleague.ml.GridTools;
@@ -17,7 +18,9 @@ import com.expleague.ml.methods.trees.GreedyObliviousTree;
 import com.expleague.ml.models.MultiClassModel;
 import org.apache.commons.cli.*;
 
+import javax.xml.crypto.Data;
 import java.io.*;
+import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
 
 public class FMCBoostingMain {
@@ -30,13 +33,19 @@ public class FMCBoostingMain {
             .hasArg()
             .argName("TRAIN")
             .type(String.class)
-            .required()
             .build());
     options.addOption(Option.builder()
             .longOpt("test")
             .desc("Path to the test dataset")
             .hasArg()
             .argName("TEST")
+            .type(String.class)
+            .build());
+    options.addOption(Option.builder()
+            .longOpt("model")
+            .desc("Path to the model")
+            .hasArg()
+            .argName("MODEL")
             .type(String.class)
             .required()
             .build());
@@ -85,17 +94,31 @@ public class FMCBoostingMain {
             .type(Number.class)
             .required()
             .build());
+    options.addOption(Option.builder()
+            .longOpt("train_pred")
+            .desc("Path to the file with predictions on training dataset")
+            .hasArg()
+            .argName("TRAIN_PRED")
+            .type(String.class)
+            .build());
+    options.addOption(Option.builder()
+            .longOpt("test_pred")
+            .desc("Path to the file with predictions on test dataset")
+            .hasArg()
+            .argName("TEST_PRED")
+            .type(String.class)
+            .build());
   }
 
   private static Trans fit(final FMCBoosting boosting, final Pool<?> train, final Pool<?> test) {
     final VecDataSet vecDataSet = train.vecData();
     final BlockwiseMLLLogit globalLoss = train.target(BlockwiseMLLLogit.class);
-    final MulticlassProgressPrinter multiclassProgressPrinter = new MulticlassProgressPrinter(train, test);
-    boosting.addListener(multiclassProgressPrinter);
-
-    Interval.start();
+    // final MulticlassProgressPrinter multiclassProgressPrinter = new MulticlassProgressPrinter(train, test);
+    // boosting.addListener(multiclassProgressPrinter);
+    long startTime = System.currentTimeMillis();
     final Ensemble ensemble = boosting.fit(vecDataSet, globalLoss);
-    Interval.stopAndPrint(" trainging");
+    Interval.setStart(startTime);
+    Interval.stopAndPrint(" training");
 
     final Trans joined = ensemble.last() instanceof FuncJoin ? MCTools.joinBoostingResult(ensemble) : ensemble;
     final MultiClassModel multiclassModel = new MultiClassModel(joined);
@@ -106,50 +129,98 @@ public class FMCBoostingMain {
     return joined;
   }
 
-  private static void eval(final Trans ensemble, final Pool<?> test) {
-    Interval.start();
+  private static Vec eval(final Trans ensemble, final Pool<?> test, final String comment) {
     final MultiClassModel multiclassModel = new MultiClassModel(ensemble);
-    final String testResult = MCTools.evalModel(multiclassModel, test, "[TEST] ", false);
+
+    Interval.start();
+    final String testResult = MCTools.evalModel(multiclassModel, test, comment + " ", false);
     System.out.println(testResult);
-    Interval.stopAndPrint(" evaluation on test");
+    Interval.stopAndPrint(" evaluation on " + comment);
+    return multiclassModel.bestClassAll(test.vecData().data());
   }
 
-  public static void main(String[] args) throws IOException, ParseException {
+  private static void saveIntVec(final Vec data, final String path) throws Exception {
+    final String result = data.stream().mapToObj(Math::round).map(Object::toString).collect(Collectors.joining(","));
+    final PrintStream out = new PrintStream(new FileOutputStream(new File(path)));
+    out.println(result);
+    out.close();
+  }
+
+  public static void main(String[] args) throws Exception {
     CommandLineParser parser = new DefaultParser();
     try {
       CommandLine cmd = parser.parse(options, args);
 
-      final String trainPath = cmd.getOptionValue("train");
-      final String testPath = cmd.getOptionValue("test");
+      final String model = cmd.getOptionValue("model", null);
+      final String trainPath = cmd.getOptionValue("train", null);
+      final String testPath = cmd.getOptionValue("test", null);
       final int iterCount = Integer.parseInt(cmd.getOptionValue("iter_count"));
       final double step = Double.parseDouble(cmd.getOptionValue("step"));
-
       final double gamma = Double.parseDouble(cmd.getOptionValue("gamma", "100"));
       final int maxIter = Integer.parseInt(cmd.getOptionValue("max_iter", "2000"));
       final int depth = Integer.parseInt(cmd.getOptionValue("depth", "5"));
       final int binFactor = Integer.parseInt(cmd.getOptionValue("bin_factor", "32"));
+      final String trainPredPath = cmd.getOptionValue("train_pred", null);
+      final String testPredPath = cmd.getOptionValue("test_pred", null);
 
       final FastRandom rng = new FastRandom(0);
 
-      final InputStreamReader trainReader = new InputStreamReader(new GZIPInputStream(new FileInputStream(trainPath)));
-      final Pool<?> train = DataTools.loadFromFeaturesTxt(trainPath, trainReader);
+      Pool<?> train = null;
+      if (trainPath != null) {
+        InputStreamReader trainReader = new InputStreamReader(new GZIPInputStream(new FileInputStream(trainPath)));
+        train = DataTools.loadFromFeaturesTxt(trainPath, trainReader);
+      }
 
-      final InputStreamReader testReader = new InputStreamReader(new GZIPInputStream(new FileInputStream(testPath)));
-      final Pool<?> test = DataTools.loadFromFeaturesTxt(testPath, testReader);
+      Pool<?> test = null;
+      if (testPath != null) {
+        final InputStreamReader testReader = new InputStreamReader(new GZIPInputStream(new FileInputStream(testPath)));
+        test = DataTools.loadFromFeaturesTxt(testPath, testReader);
+      }
 
-      final FMCBoosting boosting = new FMCBoosting(
-              new StochasticALS(rng, gamma, maxIter, new StochasticALS.Cache(600, 0.01, rng)),
-              new GreedyObliviousTree<L2>(GridTools.medianGrid(train.vecData(), binFactor), depth),
-              L2.class,
-              iterCount,
-              step
-      );
+      Trans ensemble = null;
+      if (train == null && model != null) {
+        ensemble = DataTools.readModel(new FileInputStream(new File(model)));
+      }
 
-      final Trans ensemble = fit(boosting, train, test);
-      eval(ensemble, test);
+      if (train == null && test == null) {
+        throw new IllegalArgumentException("Either train or test dataset is required!");
+      }
+
+      if (train == null && ensemble == null) {
+        throw new IllegalArgumentException("You should specify the model for test evaluation!");
+      }
+
+      if (train != null) {
+        final FMCBoosting boosting = new FMCBoosting(
+                new StochasticALS(rng, gamma, maxIter, new StochasticALS.Cache(600, 0.01, rng)),
+                new GreedyObliviousTree<L2>(GridTools.medianGrid(train.vecData(), binFactor), depth),
+                L2.class,
+                iterCount,
+                step
+        );
+
+        ensemble = fit(boosting, train, test);
+
+        if (model != null) {
+          DataTools.writeModel(ensemble, new FileOutputStream(new File(model)));
+        }
+      }
+
+      if (test != null) {
+        final Vec pred = eval(ensemble, test, "test");
+        if (testPredPath != null) {
+          saveIntVec(pred, testPredPath);
+        }
+      }
+
+      if (train != null) {
+        final Vec pred = eval(ensemble, train, "train");
+        if (trainPredPath != null) {
+          saveIntVec(pred, trainPredPath);
+        }
+      }
     } catch (Exception e) {
       throw e;
-     // System.err.println(e);
     }
   }
 }
