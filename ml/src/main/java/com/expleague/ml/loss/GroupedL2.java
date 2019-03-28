638f059179e2399f953b5250ba7cc7a0ce350094
SandMV <sandulmv@yandex.ru>
2019-03-28 10:35:57 +0300

    Reworked GroupedL2 loss

--- ml/src/main/java/com/expleague/ml/loss/GroupedL2.java
+++ ml/src/main/java/com/expleague/ml/loss/GroupedL2.java
@@ -2,42 +2,93 @@ package com.expleague.ml.loss;
 
 import com.expleague.commons.math.vectors.Vec;
 import com.expleague.commons.math.vectors.VecTools;
+import com.expleague.commons.math.vectors.impl.vectors.ArrayVec;
 import com.expleague.ml.data.set.DataSet;
 import com.expleague.ml.meta.GroupedDSItem;
-import com.expleague.ml.methods.GradientBoosting;
+import gnu.trove.map.TObjectDoubleMap;
+import gnu.trove.map.hash.TObjectDoubleHashMap;
 import gnu.trove.set.TIntSet;
 import gnu.trove.set.hash.TIntHashSet;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
+import java.util.function.BiFunction;
 import org.jetbrains.annotations.NotNull;
 
 public class GroupedL2 extends L2 {
-  // So OK, we will make GroupedL2 (which is working name btw),
-  // which loss and gradient depends on the value of 'x' not the target
 
-  private final Set<TIntSet> groups = new HashSet<>();
-
+  private final Vec groupsOffsets;
   public GroupedL2(final Vec target, final DataSet<? extends GroupedDSItem> owner) {
-    super(target, owner);
-    Map<String, TIntSet> groupIndexesMap = new HashMap<>();
-    int idx = 0;
-    for (GroupedDSItem item : owner) {
-      String groupId = item.groupId();
-      if (!groupIndexesMap.containsKey(groupId)) {
-        groupIndexesMap.put(groupId, new TIntHashSet());
-      }
-      groupIndexesMap.get(groupId).add(idx++);
-    }
-    groups.addAll(groupIndexesMap.values());
+    this(target, owner, groupMeansOffsets());
   }
 
+  //
+  public GroupedL2(final Vec target, final DataSet<? extends GroupedDSItem> owner,
+      BiFunction<Vec, DataSet<? extends GroupedDSItem>, Vec> offsetFunction) {
+    super(target, owner);
+    groupsOffsets = offsetFunction.apply(target, owner);
+  }
+
+  public static BiFunction<Vec, DataSet<? extends GroupedDSItem>, Vec> randomOffsets(
+      final double lowerBound, final double upperBound, final int seed) {
+    return (vec, ds) -> {
+
+    };
+  }
+
+  public static BiFunction<Vec, DataSet<? extends GroupedDSItem>, Vec> groupMeansOffsets() {
+    return new BiFunction<Vec, DataSet<? extends GroupedDSItem>, Vec>() {
+      private Vec lastTarget = null;
+      private DataSet<? extends GroupedDSItem> lastDataSet = null;
+      private Vec lastOffsets = null;
+
+      @Override
+      public Vec apply(Vec target, DataSet<? extends GroupedDSItem> dataSet) {
+        // just check whether we received the same values as last time
+        if (target == lastTarget && dataSet == lastDataSet) {
+          return lastOffsets;
+        }
+
+        lastTarget = target;
+        lastDataSet = dataSet;
+        lastOffsets = new ArrayVec(lastTarget.dim());
+
+        Map<String, TIntSet> groups = new HashMap<>();
+        TObjectDoubleMap<String> groupSums = new TObjectDoubleHashMap<>();
+        int idx = 0;
+        for (GroupedDSItem item : dataSet) {
+          String groupId = item.groupId();
+          if (!groups.containsKey(groupId)) {
+            groups.put(groupId, new TIntHashSet());
+          }
+          groups.get(groupId).add(idx);
+
+          double targetAtIdx = target.get(idx);
+          groupSums.adjustOrPutValue(groupId, targetAtIdx, targetAtIdx);
+          ++idx;
+        }
+
+        groups.forEach((groupId, groupIndexes) -> {
+          double groupMean = groupSums.get(groupId) / groupIndexes.size();
+          groupIndexes.forEach(i -> {
+            lastOffsets.set(i, groupMean);
+            return true;
+          });
+        });
+
+        return VecTools.copy(lastOffsets);
+      }
+    };
+  }
+
+  public static BiFunction<Vec, DataSet<? extends GroupedDSItem>, Vec> constantOffsets(
+      final Vec offsets) {
+    final Vec safeCopy = VecTools.copy(offsets);
+    return (v, ds) -> VecTools.copy(safeCopy);
+  }
   @NotNull
   @Override
   public Vec gradient(final Vec x) {
     Vec gradient = VecTools.copy(x);
-    offsetByGroupMeans(gradient);
     VecTools.scale(gradient, -1);
     VecTools.append(gradient, target);
     VecTools.scale(gradient, -2);
@@ -58,22 +109,6 @@ public class GroupedL2 extends L2 {
           }
       );
     }
-  }
-
-  private static double groupMean(final Vec x, final TIntSet group) {
-    if (group.isEmpty()) {
-      throw new IllegalArgumentException("Group must not be empty!");
-    }
-
-    double[] sum = {0};
-    // TODO: can produce 'index out of bound exception' or smth like this
-    group.forEach(i -> {
-          sum[0] += x.get(i);
-          return true;
-        }
-    );
-
-    return sum[0] / group.size();
   }
 
   @Override
