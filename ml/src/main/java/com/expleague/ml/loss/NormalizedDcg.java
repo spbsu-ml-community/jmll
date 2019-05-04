b244bcea0ec73c5cad961a0b6cbede7fb77032f6
SandMV <sandulmv@yandex.ru>
2019-05-04 14:03:55 +0300

    Updated NormalizedDcg to work properly with groups rith identical labels

--- ml/src/main/java/com/expleague/ml/loss/NormalizedDcg.java
+++ ml/src/main/java/com/expleague/ml/loss/NormalizedDcg.java
@@ -13,14 +13,18 @@ import gnu.trove.list.array.TIntArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NormalizedDcg extends Func.Stub implements TargetFunc {
+    private static final Logger LOG = LoggerFactory.getLogger(NormalizedDcg.class);
     private static final double LOG_2 = Math.log(2.);
+    private static final double EPSILON = 1e-9;
     private final DataSet<? extends GroupedDSItem> owner;
     private final Vec target;
     private final Group[] groups;
 
-    public NormalizedDcg(Vec target, DataSet<? extends GroupedDSItem> owner) {
+    public NormalizedDcg(final Vec target, final DataSet<? extends GroupedDSItem> owner) {
         this.owner = owner;
         this.target = target;
 
@@ -36,11 +40,30 @@ public class NormalizedDcg extends Func.Stub implements TargetFunc {
 
         groups = new Group[groupsMap.size()];
         idx = 0;
-        for (TIntList groupIds : groupsMap.values()) {
-            int[] groupIdsArray = groupIds.toArray();
+        for (Map.Entry<String, TIntList> groupEntry : groupsMap.entrySet()) {
+            int[] groupIdsArray = groupEntry.getValue().toArray();
+            if (!hasDifferentLabels(target, groupIdsArray)) {
+                LOG.warn(String.format("In the group with id [ %s ] all labels are the same!",
+                    groupEntry.getKey()));
+                continue;
+            }
             double groupDcg = groupDcg(target, target, groupIdsArray);
             groups[idx++] = new Group(groupDcg, groupIdsArray);
         }
+        
+        if (idx == 0) {
+            throw new IllegalStateException("Received target labels has no different values!");
+        }
+    }
+
+    private static boolean hasDifferentLabels(final Vec target, final int[] groupIds) {
+        double label = target.get(groupIds[0]);
+        for (int i : groupIds) {
+            if (Math.abs(label - target.get(i)) > EPSILON) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static double dcg(final int[] labelsOrder, final Vec targetLabels) {
